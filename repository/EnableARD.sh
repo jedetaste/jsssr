d14a0e58a615ce29c2599d14108efb9f957d5eae
jedetaste <office@anykeyit.ch>
2017-07-20 10:34:09 +0200

    Update EnableARD.sh

--- repository/EnableARD.sh
+++ repository/EnableARD.sh
@@ -3,14 +3,18 @@
 	kickstart="/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart"
 	
 	if [ ! -z "$(id -u gucken)" ]; then
+	
 		echo "User id gucken exists"
 		"${kickstart}" -activate -configure -allowAccessFor -specifiedUsers
 		"${kickstart}" -configure -users admin,gucken -access -on -privs -all
 		/usr/bin/defaults write /Library/Preferences/com.apple.loginwindow HiddenUsersList -array-add gucken
+		
 	else
+	
 		echo "User id gucken does not exist"
 		"${kickstart}" -activate -configure -allowAccessFor -specifiedUsers
 		"${kickstart}" -configure -users admin -access -on -privs -all
+		
 	fi
 	
 exit 0
