6287e097d58b911606ee8eed182c26b501ff48b5
Anver Housseini <contact@anver.ch>
2018-01-03 09:36:29 +0100

    Update RenameBootVolume.sh

--- repository/RenameBootVolume.sh
+++ repository/RenameBootVolume.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-	BootVolume=$(diskutil info / | grep "Volume Name" | cut -c 30-)
-	
-	if [ "$VolumeName" != "Macintosh HD" ]; then
-		diskutil renameVolume "$BootVolume" "Macintosh HD"
-	fi
-	
-	BootVolume=$(diskutil info / | grep "Volume Name" | cut -c 30-)
-	
-	echo "<result>${BootVolume}</result>"
-	
-exit 0
+BootVolume=$(/usr/sbin/diskutil info / | grep "Volume Name" | cut -c 30-)
+
+if [ "${VolumeName}" != "Macintosh HD" ]; then
+  /usr/sbin/diskutil renameVolume "${BootVolume}" "Macintosh HD"
+fi
+
+BootVolume=$(/usr/sbin/diskutil info / | grep "Volume Name" | cut -c 30-)
+
+echo "<result>${BootVolume}</result>"
