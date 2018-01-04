d180b9fd48bf89afa468958be833658608479204
Anver Housseini <contact@anver.ch>
2018-01-04 09:41:42 +0100

    Update

--- Library/Enable ARD.sh
+++ Library/Enable ARD.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-  # Re-Configure ARD and activate for User admin and gucken 
+# Re-Configure ARD and activate for User admin and gucken 
+
+kickstart="/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart"
+
+if [ ! -z "$(id -u gucken)" ]; then
   
-  kickstart="/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart"
+  echo "User id gucken exists"
   
-  if [ ! -z "$(id -u gucken)" ]; then
-    echo "User id gucken exists"
-    "${kickstart}" -activate -configure -allowAccessFor -specifiedUsers
-    "${kickstart}" -configure -users admin,gucken -access -on -privs -all
-    /usr/bin/defaults write /Library/Preferences/com.apple.loginwindow HiddenUsersList -array-add gucken
-  else
-    echo "User id gucken does not exist"
-    "${kickstart}" -activate -configure -allowAccessFor -specifiedUsers
-    "${kickstart}" -configure -users admin -access -on -privs -all
-  fi
+  "${kickstart}" -activate -configure -allowAccessFor -specifiedUsers
+  "${kickstart}" -configure -users admin,gucken -access -on -privs -all
   
-  # Deactivate Screen Sharing Request Login
+  /usr/bin/defaults write /Library/Preferences/com.apple.loginwindow HiddenUsersList -array-add gucken
+
+else
+ 
+  echo "User id gucken does not exist"
   
-  /usr/bin/defaults write /Library/Preferences/com.apple.RemoteManagement ScreenSharingReqPermEnabled -bool NO
+  "${kickstart}" -activate -configure -allowAccessFor -specifiedUsers
+  "${kickstart}" -configure -users admin -access -on -privs -all
+  
+fi
+
+# Deactivate Screen Sharing Request Login
+
+/usr/bin/defaults write "/Library/Preferences/com.apple.RemoteManagement" ScreenSharingReqPermEnabled -bool NO
