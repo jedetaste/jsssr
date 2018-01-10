1a3f925ae77b4d1ee12fd43bed8c88bb1681a313
Anver Housseini <contact@anver.ch>
2018-01-10 10:04:24 +0100

    Update Library

--- Library/Install Keynote.sh
+++ Library/Install Keynote.sh
@@ -3,69 +3,69 @@
   localCDN8080=$(/usr/bin/curl -sL -w "%{http_code}" "http://cdn.anykeyit.ch:8080" -o /dev/null)
   localCDN80=$(/usr/bin/curl -sL -w "%{http_code}" "http://cdn.anykeyit.ch" -o /dev/null)
   cloudCDN=$(/usr/bin/host cdn.anykeyit.ch | /usr/bin/awk '{print $NF}')
+  
+  osvers=$(/usr/bin/sw_vers -productVersion | awk -F. '{print $2}')
 
-  if [[ "${localCDN8080}" == "200" ]]; then
+  if [ "${localCDN8080}" == "200" ]; then
 
     startTime=$(date +"%s")
 
     echo "CDN resolves to a local IP. Continuing..."
-
-    osvers=$(/usr/bin/sw_vers -productVersion | awk -F. '{print $2}')
-
-    if [[ "${osvers}" -eq 9 ]]; then
+    
+    if [ "${osvers}" -eq 9 ]; then
       echo "Upgrading Keynote for Mavericks"
       /usr/local/bin/aky keynote10098080
     fi
 
-    if [[ "${osvers}" -eq 10 ]]; then
+    if [ "${osvers}" -eq 10 ]; then
       echo "Upgrading Keynote for Yosemite"
       /usr/local/bin/aky keynote10108080
     fi
 
-    if [[ "${osvers}" -eq 11 ]]; then
+    if [ "${osvers}" -eq 11 ]; then
       echo "Upgrading Keynote for El Capitain"
       /usr/local/bin/aky keynote10118080
     fi
 
-    if [[ "${osvers}" -eq 12 ]]; then
+    if [ "${osvers}" -eq 12 ]; then
       echo "Upgrading Keynote for Sierra"
       /usr/local/bin/aky keynote10128080
     fi
 
-    if [[ "${osvers}" -eq 13 ]]; then
+    if [ "${osvers}" -eq 13 ]; then
       echo "Upgrading Keynote for High Sierra"
       /usr/local/bin/aky keynote10138080
     fi
 
     endTime=$(date +"%s") && echo "Run time: $((endTime-startTime)) seconds..."
 
-  elif [[  "${localCDN80}" == "200" ] || [ "${cloudCDN}" != "217.150.247.87"  ]]; then
+  elif [ "${localCDN80}" == "200" ] || [ "${cloudCDN}" != "217.150.247.87"  ]; then
 
     startTime=$(date +"%s")
 
     echo "CDN resolves to a local IP. Continuing..."
 
-    if [[ "${osvers}" -eq 9 ]]; then
+    if [ "${osvers}" -eq 9 ]; then
       echo "Upgrading Keynote for Mavericks"
       /usr/local/bin/aky keynote1009
     fi
 
-    if [[ "${osvers}" -eq 10 ]]; then
+    if [ "${osvers}" -eq 10 ]; then
       echo "Upgrading Keynote for Yosemite"
       /usr/local/bin/aky keynote1010
     fi
 
-    if [[ "${osvers}" -eq 11 ]]; then
+    if [ "${osvers}" -eq 11 ]; then
       echo "Upgrading Keynote for El Capitain"
       /usr/local/bin/aky keynote1011
     fi
 
-    if [[ "${osvers}" -eq 12 ]]; then
+    if [ "${osvers}" -eq 12 ]; then
       echo "Upgrading Keynote for Sierra"
       /usr/local/bin/aky keynote1012
     fi
 
-    if [[ "${osvers}" -eq 13 ]]; then
+    if [ "${osvers}" -eq 13 ]; then
       echo "Upgrading Keynote for High Sierra"
       /usr/local/bin/aky keynote1013
     fi
