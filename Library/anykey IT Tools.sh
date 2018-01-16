b4f57b9a6666e0224f81351d6aab9c98a8e81050
Anver Housseini <contact@anver.ch>
2018-01-16 23:10:53 +0100

    Update Library

--- Library/anykey IT Tools.sh
+++ Library/anykey IT Tools.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
-  
-  # appleLoops
-  
-  /usr/bin/curl -sfko "/usr/local/bin/appleLoops" "https://raw.githubusercontent.com/carlashley/appleLoops/master/appleLoops.py"
-  
-  /usr/sbin/chown root:wheel "/usr/local/bin/appleLoops"
-  /bin/chmod 775 "/usr/local/bin/appleLoops"
-  /bin/chmod +x "/usr/local/bin/appleLoops"
-  
-  # dockutil
-  
-  /usr/bin/curl -sfko "/usr/local/bin/dockutil" "https://github.com/kcrawford/dockutil/blob/master/scripts/dockutil"
-  
-  /usr/sbin/chown root:wheel "/usr/local/bin/dockutil"
-  /bin/chmod 775 "/usr/local/bin/dockutil"
-  /bin/chmod +x "/usr/local/bin/dockutil"
+
+	# appleLoops
+	
+	if [ -s "/usr/local/bin/appleLoops" ]; then
+		rm -f "/usr/local/bin/appleLoops"
+	fi
+	
+	/usr/bin/curl -sfko "/usr/local/bin/appleLoops" "https://raw.githubusercontent.com/carlashley/appleLoops/master/appleLoops.py"
+
+	/usr/sbin/chown root:wheel "/usr/local/bin/appleLoops"
+	/bin/chmod 775 "/usr/local/bin/appleLoops"
+	/bin/chmod +x "/usr/local/bin/appleLoops"
+
+	# dockutil
+	
+	if [ -s "/usr/local/bin/dockutil" ]; then
+		rm -f "/usr/local/bin/dockutil"
+	fi
+	
+	/usr/bin/curl -sfko "/usr/local/bin/dockutil" "https://raw.githubusercontent.com/kcrawford/dockutil/master/scripts/dockutil"
+
+	/usr/sbin/chown root:wheel "/usr/local/bin/dockutil"
+	/bin/chmod 775 "/usr/local/bin/dockutil"
+	/bin/chmod +x "/usr/local/bin/dockutil"
