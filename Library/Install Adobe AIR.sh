cf065212096f59fa6363c3d0d85354e7524b30ab
Anver Housseini <contact@anver.ch>
2018-01-22 10:21:01 +0100

    Update Library

--- Library/Install Adobe AIR.sh
+++ Library/Install Adobe AIR.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
   
-  versionInstaller="458.0.1"
-  downloadInstaller="https://scratch.mit.edu/scratchr2/static/sa/Scratch-${versionInstaller}.dmg"
+  versionInstaller="28.0"
+  downloadInstaller="https://airdownload.adobe.com/air/mac/download/${versionInstaller}/AdobeAIR.dmg"
   
   tmpFolder=$(getconf DARWIN_USER_CACHE_DIR) && randString=$(/usr/bin/openssl rand -hex 5) && tmpDir="${tmpFolder}${randString}" && /bin/mkdir -p "${tmpDir}"
   
@@ -21,9 +21,9 @@
   
   installerVolume=$(/usr/bin/hdiutil attach -noautoopen -noverify -nobrowse "${filePath}.cdr" | egrep "Volumes" | grep -o "/Volumes/.*")
   
-  echo "Running installer at '${installerVolume}/Install Scratch 2.app/Contents/MacOS/Install Scratch 2'"
+  echo "Running installer at '${installerVolume}/Adobe AIR Installer.app/Contents/MacOS/Adobe AIR Installer'"
   
-  "${installerVolume}/Install Scratch 2.app/Contents/MacOS/Install Scratch 2" -silent
+  "${installerVolume}/Adobe AIR Installer.app/Contents/MacOS/Adobe AIR Installer" -silent
   
   /usr/sbin/diskutil quiet eject "${installerVolume}" && sleep 2
   
