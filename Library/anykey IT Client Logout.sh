6dc16945a5b7968a69f94ac192ffb3404bc70fcb
Anver Housseini <contact@anver.ch>
2018-01-24 21:31:11 +0100

    Update Library

--- Library/anykey IT Client Logout.sh
+++ Library/anykey IT Client Logout.sh
@@ -1,95 +1,44 @@
 #!/bin/bash
-
-  # Set permissions on Accounts.prefPane
-
-  if [ -d "/Users/gastschule/" ]; then
-    pwpolicy -u gastschule -setpolicy "canModifyPasswordforSelf=0"
-    echo Benutzer gastschule gefunden und Kennwort Aenderung deaktiviert
-  fi
-
-  if [ -d "/Users/lernende/" ]; then
-    pwpolicy -u lernende -setpolicy "canModifyPasswordforSelf=0"
-    echo Benutzer lernende gefunden und Kennwort Aenderung deaktiviert
-  fi
-
-  if [ -d "/Users/lokal/" ]; then
-    pwpolicy -u lokal -setpolicy "canModifyPasswordforSelf=0"
-    echo Benutzer lokal gefunden und Kennwort Aenderung deaktiviert
-  fi
-
-  if [ -d "/Users/mittelstufe/" ]; then
-    pwpolicy -u mittelstufe -setpolicy "canModifyPasswordforSelf=0"
-    echo Benutzer mittelstufe gefunden und Kennwort Aenderung deaktiviert
-  fi
-
-  if [ -d "/Users/schueler/" ]; then
-    pwpolicy -u schueler -setpolicy "canModifyPasswordforSelf=0"
-    echo Benutzer schueler gefunden und Kennwort Aenderung deaktiviert
-  fi
-
-  if [ -d "/Users/sus/" ]; then
-    pwpolicy -u sus -setpolicy "canModifyPasswordforSelf=0"
-    echo Benutzer sus gefunden und Kennwort Aenderung deaktiviert
-  fi
-
-  if [ -d "/Users/unterstufe/" ]; then
-    pwpolicy -u unterstufe -setpolicy "canModifyPasswordforSelf=0"
-    echo Benutzer unterstufe gefunden und Kennwort Aenderung deaktiviert
-  fi
   
-  if [ -d "/Users/schule/" ]; then
-    pwpolicy -u schule -setpolicy "canModifyPasswordforSelf=0"
-    echo Benutzer schule gefunden und Kennwort Aenderung deaktiviert
-  fi
-
+  userName=(
+    'gastschule'
+    'lernende'
+    'lokal'
+    'mittelstufe'
+    'unterstufe'
+    'schueler'
+    'sus'
+    'schule'
+  )
+  
+  # Set permissions on Accounts.prefPane
+  
+  for ((i = 0; i < "${#userName[@]}"; i++)); do
+    
+    if [ -d "/Users/${#userName[@]}" ]; then
+      /usr/bin/pwpolicy -u "${#userName[@]}" -setpolicy "canModifyPasswordforSelf=0"
+      echo "Set password policy for user ${#userName[@]}"
+    fi
+    
+  done
+  
   # Create directory geloeschteHomes in Library
-
+  
   if [ ! -d "/Library/geloeschteHomes/" ]; then
     mkdir /Library/geloeschteHomes
   fi
-
-  # Move home directory schueler to Library
-
-  if [ -d "/Users/gastschule/" ]; then
-    mv /Users/gastschule /Library/geloeschteHomes/gastschule-$(date +%Y-%m-%d--%H-%M-%S)
-    echo Benutzerordner gastschule gefunden und Benutzerordner archiviert
-  fi
-
-  if [ -d "/Users/lernende/" ]; then
-    mv /Users/lernende /Library/geloeschteHomes/lernende-$(date +%Y-%m-%d--%H-%M-%S)
-    echo Benutzerordner lernende gefunden und Benutzerordner archiviert
-  fi
-
-  if [ -d "/Users/lokal/" ]; then
-    mv /Users/lokal /Library/geloeschteHomes/lokal-$(date +%Y-%m-%d--%H-%M-%S)
-    echo Benutzerordner lokal gefunden und Benutzerordner archiviert
-  fi
-
-  if [ -d "/Users/mittelstufe/" ]; then
-    mv /Users/mittelstufe /Library/geloeschteHomes/mittelstufe-$(date +%Y-%m-%d--%H-%M-%S)
-    echo Benutzerordner mittelstufe gefunden und Benutzerordner archiviert
-  fi
-
-  if [ -d "/Users/schueler/" ]; then
-    mv /Users/schueler /Library/geloeschteHomes/schueler-$(date +%Y-%m-%d--%H-%M-%S)  
-    echo Benutzerordner schueler gefunden und Benutzerordner archiviert
-  fi
-
-  if [ -d "/Users/sus/" ]; then
-    mv /Users/sus /Library/geloeschteHomes/sus-$(date +%Y-%m-%d--%H-%M-%S)
-    echo Benutzerordner sus gefunden und Benutzerordner archiviert
-  fi
-
-  if [ -d "/Users/unterstufe/" ]; then
-    mv /Users/unterstufe /Library/geloeschteHomes/unterstufe-$(date +%Y-%m-%d--%H-%M-%S)
-    echo Benutzerordner unterstufe gefunden und Benutzerordner archiviert
-  fi
   
-  if [ -d "/Users/schule/" ]; then
-    mv /Users/schule /Library/geloeschteHomes/schule-$(date +%Y-%m-%d--%H-%M-%S)
-    echo Benutzerordner schule gefunden und Benutzerordner archiviert
-  fi
-
+  # Move home directory to Library
+  
+  for ((i = 0; i < "${#userName[@]}"; i++)); do
+    
+    if [ -d "/Users/${#userName[@]}" ]; then
+      mv "/Users/${#userName[@]}" "/Library/geloeschteHomes/${#userName[@]}-$(date +%Y-%m-%d--%H-%M-%S)"
+      echo "Move home directory '/Users/${#userName[@]}' to Library"
+    fi
+    
+  done
+  
   # Set permissions
   
   chown -R admin "/Library/geloeschteHomes/"
@@ -114,5 +63,3 @@
   # Repair shared folder
   
   chmod -R 777 "/Users/Shared/"
-
-exit 0
