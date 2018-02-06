8332831d3bb34b0404a2b01b5f66a0539ffbe722
Anver Housseini <contact@anver.ch>
2018-02-06 20:59:19 +0100

    Update Library

--- Library/Set PDF UTI Default Application.sh
+++ Library/Set PDF UTI Default Application.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-	
-	currentUser=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
-	
-	su -l "${currentUser}" -c "/usr/local/bin/duti -s com.adobe.Reader com.adobe.pdf all"
-	su -l "${currentUser}" -c "/usr/local/bin/duti -s com.adobe.Reader public.composite-content all"
-	
-exit 0
+  
+  currentUser=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+  
+  su -l "${currentUser}" -c "/usr/local/bin/duti -s com.adobe.Reader com.adobe.pdf all"
+  su -l "${currentUser}" -c "/usr/local/bin/duti -s com.adobe.Reader public.composite-content all"
