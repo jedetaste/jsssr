0adbe769130b408ed91c84fa2e740feb8bf4f5f7
Anver Housseini <contact@anver.ch>
2018-02-08 22:37:31 +0100

    Update Library

---
+++ Library/JAMF Create Account.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+  
+  username="${1}"
+  realname="${2}"
+  password="${3}"
+  home="${4}"
+  admin="${5}"
+  
+  jamf="/usr/local/bin/jamf"
+  
+  if [ ! -z "$(id -u ${username} 2>/dev/null)" ]; then
+  
+    echo "User ${username} already exists"
+  
+  else
+    
+    echo "User ${username} does not exist"
+    
+    if [ ! -z "${admin}" ]; then
+      
+      "${jamf}" createAccount \
+        -username "${username}" \
+        -realname "${realname}" \
+        -password "${password}" \
+        -home "/Users/${home}" \
+        -suppressSetupAssistant \
+        -admin
+      
+      /usr/bin/dscl . delete /Users/${username} jpegphoto
+      /usr/bin/dscl . delete /Users/${username} Picture
+      
+    elif [ -z "${admin}" ]; then
+      
+      "${jamf}" createAccount \
+        -username "${username}" \
+        -realname "${realname}" \
+        -password "${password}" \
+        -home "/Users/${home}" \
+        -suppressSetupAssistant
+      
+      /usr/bin/dscl . delete /Users/${username} jpegphoto
+      /usr/bin/dscl . delete /Users/${username} Picture
+      
+    fi
+  
+  fi
