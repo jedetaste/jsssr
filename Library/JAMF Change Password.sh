5125d58a546dbf6c5a7bce8f770c90d8185eac45
Anver Housseini <contact@anver.ch>
2018-02-08 11:21:05 +0100

    Add Argument Parsing

---
+++ Library/JAMF Change Password.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+	
+	username="${1}"
+	password="${2}"
+	oldPassword="${3}"
+	
+	jamf="/usr/local/jamf/bin/jamf"
+	
+	"${jamf}" changePassword \
+		-username "${username}" \
+		-password "${password}" \
+		-oldPassword "${oldPassword}"
