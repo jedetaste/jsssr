559cdda1b72bed1e2d233b166e1a7c783074d5a4
Anver Housseini <contact@anver.ch>
2018-02-08 11:23:19 +0100

    Update Library

--- Library/JAMF Change Password.sh
+++ Library/JAMF Change Password.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-	
-	username="${1}"
-	password="${2}"
-	oldPassword="${3}"
-	
-	jamf="/usr/local/jamf/bin/jamf"
-	
-	"${jamf}" changePassword \
-		-username "${username}" \
-		-password "${password}" \
-		-oldPassword "${oldPassword}"
+  
+  username="${1}"
+  password="${2}"
+  oldPassword="${3}"
+  
+  jamf="/usr/local/jamf/bin/jamf"
+  
+  "${jamf}" changePassword \
+    -username "${username}" \
+    -password "${password}" \
+    -oldPassword "${oldPassword}"
