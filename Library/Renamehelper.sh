58d196872aaf6983f003c2f020049e743a31136f
cgardini99 <c.gardini@anykeyit.ch>
2018-07-11 13:02:43 +0200

    Update Renamehelper.sh

--- Library/Renamehelper.sh
+++ Library/Renamehelper.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+######################################################################################################
+# Crated by Claudio Gardini | anykeyIT 
+# Use this Script with JAMF Pro to Rename Computers
+# There are three ways of using it
+#
+# 1. Create a CSV containing Serialnumber and Devicename and save it as /private/tmp/computernames.csv on all Machine you would like to rename. 
+# Run the Script using "/usr/local/bin/jsssr "Renamehelper.sh"" and it will set the Devicename using the corresponding Serial
+#
+# 2. Host the CSV File online and enter the URL as the first Argument. eg. /usr/local/bin/jsssr "Renamehelper.sh" "https://ewolke.ch/index.php/s/asyjZ78Zt01jyyB/download"
+#
+# 3. Use "/usr/local/bin/jsssr "Renamehelper.sh" "serialnumber" to set the Serial Number as Device Name. You can optionally enter Prefix or Suffix
+# eg. usr/local/bin/jsssr "Renamehelper.sh" "serialnumber" "Mac_" "_anykey" gives the Devicename "Mac_C02RQ1BFFVH7_anykey"
+######################################################################################################
+
   readonly csvURL="${1}"
   readonly prefix="${2}"
   readonly suffix="${3}"
