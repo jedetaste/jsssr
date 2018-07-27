a85842f5355a2e77b2e31907ffb2874569594782
cgardini99 <c.gardini@anykeyit.ch>
2018-07-27 11:24:33 +0200

    Update anykey IT Client Tools.sh

--- Library/anykey IT Client Tools.sh
+++ Library/anykey IT Client Tools.sh
@@ -8,7 +8,7 @@
     rm -f "/usr/local/bin/appleLoops"
   fi
   
-  /usr/bin/curl -sfko "/usr/local/bin/appleLoops" "https://raw.githubusercontent.com/carlashley/appleLoops/master/appleLoops.py"
+  /usr/bin/curl -so "/usr/local/bin/appleLoops" "https://raw.githubusercontent.com/carlashley/appleLoops/master/appleLoops.py"
 
   /usr/sbin/chown root:wheel "/usr/local/bin/appleLoops"
   /bin/chmod 775 "/usr/local/bin/appleLoops"
