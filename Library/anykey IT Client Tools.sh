2654c44f417fd3d05787cfb04fca0be66b7ee51c
Anver Housseini <contact@anver.ch>
2018-08-16 20:08:52 +0200

    Update anykey IT Client Tools.sh

--- Library/anykey IT Client Tools.sh
+++ Library/anykey IT Client Tools.sh
@@ -161,14 +161,12 @@
   )
   
   for ((i = 0; i < "${#akyBinary[@]}"; i++)); do
-    
     /bin/rm -f "/usr/local/bin/${akyBinary[$i]}"
     /bin/rm -f "/usr/local/aky/${akyBinary[$i]}"
     /usr/bin/curl -so "/usr/local/aky/${akyBinary[$i]}" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/${akyBinary[$i]}"
     /usr/sbin/chown root:wheel "/usr/local/aky/${akyBinary[$i]}"
     /bin/chmod +x "/usr/local/aky/${akyBinary[$i]}"
     /bin/ln -s "/usr/local/aky/${akyBinary[$i]}" "/usr/local/bin/${akyBinary[$i]}"
-    
   done
   
   # Remove2011
