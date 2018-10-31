ed1f8fcc8847530b36b2ac8e9bb67f5464fe646b
Anver Housseini <contact@anver.ch>
2018-11-01 00:46:31 +0100

    Update Library/anykey IT Client Standards.sh

--- Library/anykey IT Client Standards.sh
+++ Library/anykey IT Client Standards.sh
@@ -445,6 +445,12 @@
   echo "AutoUpdateDisable=1" > "/Library/Application Support/Macromedia/mms.cfg" 2>/dev/null
   echo "SilentAutoUpdateEnable=0" >> "/Library/Application Support/Macromedia/mms.cfg" 2>/dev/null
   echo "DisableAnalytics=1" >> "/Library/Application Support/Macromedia/mms.cfg" 2>/dev/null
+  
+  # Disable Auto Update Adobe Acrobat Reader DC
+  
+  if [ -d "/Applications/Adobe Acrobat Reader DC.app/Contents/Plugins/Updater.acroplugin" ]; then
+    rm -rf "/Applications/Adobe Acrobat Reader DC.app/Contents/Plugins/Updater.acroplugin"
+  fi
 
   # Delete Adobe Reader PlugIns
   
