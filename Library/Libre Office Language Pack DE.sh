00b2a5db450374215eeffe981311fa1349581695
Anver Housseini <contact@anver.ch>
2018-11-06 12:59:57 +0100

    Update Library/Libre Office Language Pack DE.sh

--- Library/Libre Office Language Pack DE.sh
+++ Library/Libre Office Language Pack DE.sh
@@ -3,7 +3,7 @@
   tmp="/private/tmp/libreoffice" && /bin/mkdir -p "${tmp}"
   
   language="de"
-  version="6.1.2"
+  version="6.1.3"
   url="http://download.documentfoundation.org/libreoffice/stable/${version}/mac/x86_64/LibreOffice_${version}_MacOS_x86-64_langpack_${language}.dmg"
   
   echo "==> Download '${url}'"
