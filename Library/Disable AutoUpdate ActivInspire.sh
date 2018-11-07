1229b7678864ca6e9ed4a566c325b2143f985aa2
Anver Housseini <contact@anver.ch>
2018-11-07 11:46:07 +0100

    Add Disable AutoUpdate ActivInspire.sh

---
+++ Library/Disable AutoUpdate ActivInspire.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+  
+  config_file="/Applications/Promethean/Activsoftware Inspire/Inspire.app/Contents/MacOs/admin.ini"
+  
+  if [ -d "/Applications/Promethean" ]; then
+    echo > "${config_file}"
+    
+    cat > "${config_file}" <<EOL
+[General]
+
+0=
+1=
+2=
+3=
+4=
+5=
+6=
+7=
+8=
+9=
+10=
+11=1
+EOL
+  
+    if [ -s "${config_file}" ]; then
+      echo "Successfully created configuration file in '${config_file}'" && exit 0
+    else
+      echo "Something went wrong, while creating configuration file in '${config_file}'" && exit 1
+    fi
+    
+  fi
