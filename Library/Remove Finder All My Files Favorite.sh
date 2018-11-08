93e0898daa68e6d0c7a3478afc4ec49e26b0fabd
Anver Housseini <contact@anver.ch>
2018-11-08 15:46:10 +0100

    Update Remove Finder All My Files Favorite.sh

--- Library/Remove Finder All My Files Favorite.sh
+++ Library/Remove Finder All My Files Favorite.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-  currentUser=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+  currentuser=$(/usr/sbin/scutil <<< "show State:/Users/ConsoleUser" | awk -F': ' '/[[:space:]]+Name[[:space:]]:/ { if ( $2 != "loginwindow" ) { print $2 }}')
 
   if [ -z "${currentuser}" ]; then
-    
-    su -l "${currentUser}" -c '/usr/local/bin/mysides remove "All My Files"'
-    su -l "${currentUser}" -c '/usr/local/bin/mysides remove "Alle meine Dateien"'
-    
+    su -l "${currentuser}" -c "/usr/local/bin/mysides remove 'All My Files'"
+    su -l "${currentuser}" -c "/usr/local/bin/mysides remove 'Alle meine Dateien'"
   fi
