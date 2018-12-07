9da49bbe21e6799fef927af1130da3adb738e8f3
Anver Housseini <contact@anver.ch>
2018-12-07 22:44:26 +0100

    Update Install aky Patch Management.sh

--- Library/Install aky Patch Management.sh
+++ Library/Install aky Patch Management.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-
-  if [ ! -d "/usr/local/aky" ]; then
-    /bin/mkdir -p "/usr/local/aky"
+  
+  # Remove existing aky binaries
+  
+  if [ -d "/usr/local/aky/" ]; then
+    rm -rf "/usr/local/aky/"
   fi
   
-  if [ ! -d "/usr/local/bin" ]; then
-    /bin/mkdir -p "/usr/local/bin"
-  fi
-  
-  akyBinary=(
+  aky_binary=(
     "aky"
     "aria2c@el_capitan"
     "aria2c@high_sierra"
@@ -23,11 +21,34 @@
     "versioning-helper"
   )
   
-  for ((i = 0; i < "${#akyBinary[@]}"; i++)); do
-    /bin/rm -f "/usr/local/bin/${akyBinary[$i]}"
-    /bin/rm -f "/usr/local/aky/${akyBinary[$i]}"
-    /usr/bin/curl -so "/usr/local/aky/${akyBinary[$i]}" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/${akyBinary[$i]}"
-    /usr/sbin/chown root:wheel "/usr/local/aky/${akyBinary[$i]}"
-    /bin/chmod +x "/usr/local/aky/${akyBinary[$i]}"
-    /bin/ln -s "/usr/local/aky/${akyBinary[$i]}" "/usr/local/bin/${akyBinary[$i]}"
+  for ((i = 0; i < "${#aky_binary[@]}"; i++)); do
+    
+    [ -L "/usr/local/bin/${aky_binary[$i]}" ] && unlink "/usr/local/bin/${aky_binary[$i]}"
+    
+    if [ -s "/usr/local/bin/${aky_binary[$i]}" ]; then
+      rm -f "/usr/local/bin/${aky_binary[$i]}"
+    fi
+    
+  done
+  
+  # Install aky binaries
+  
+  if [ ! -d "/usr/local/bin" ]; then
+    /bin/mkdir -p "/usr/local/bin"
+  fi
+  
+  aky_binary=(
+    "aky"
+    "rg"
+    "csc"
+    "jq"
+    "aria2c"
+    "tmpDir"
+  )
+  
+  for ((i = 0; i < "${#aky_binary[@]}"; i++)); do
+    /bin/rm -f "/usr/local/bin/${aky_binary[$i]}"
+    /usr/bin/curl -so "/usr/local/bin/${aky_binary[$i]}" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/aky-src/${aky_binary[$i]}"
+    /usr/sbin/chown root:wheel "/usr/local/bin/${aky_binary[$i]}"
+    /bin/chmod +x "/usr/local/bin/${aky_binary[$i]}"
   done
