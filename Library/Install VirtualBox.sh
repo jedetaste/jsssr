ec07b2c548ac61dd9f385268b613721a0abcc0a8
Anver Housseini <contact@anver.ch>
2019-01-03 19:50:20 +0100

    Update Install VirtualBox.sh

--- Library/Install VirtualBox.sh
+++ Library/Install VirtualBox.sh
@@ -3,5 +3,5 @@
 	/usr/local/bin/aky virtualbox
 	
 	for bin in VirtualBox VirtualBoxVM VBoxNetAdpCtl VBoxNetDHCP VBoxNetNAT VBoxHeadless; do
-	  /bin/chmod u+s "/Applications/VirtualBox.app/Contents/MacOS/${bin}"
+		/bin/chmod u+s "/Applications/VirtualBox.app/Contents/MacOS/${bin}"
 	done
