7c5b4b6c24637bb0e0a361fd71b90727a6e2a9f8
Anver Housseini <contact@anver.ch>
2019-01-06 15:30:39 +0100

    Update Install macOS Installer.sh

--- Library/Install macOS Installer.sh
+++ Library/Install macOS Installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
   
-  latest_macos="10.14"
+  latest_macos="10.14.2"
   
   if [ -s "/usr/local/bin/erase-install" ]; then
-    /usr/local/bin/erase-install --move --version=${latest_macos}
+    /usr/local/bin/erase-install --version=${latest_macos} --move 
   fi
