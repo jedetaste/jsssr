2f9d2ec9a4bba979a66d9ab22e9fd21bc63e188d
Anver Housseini <contact@anver.ch>
2019-01-06 15:08:27 +0100

    Update Install GarageBand Loops.sh

--- Library/Install GarageBand Loops.sh
+++ Library/Install GarageBand Loops.sh
@@ -32,12 +32,12 @@
         
         # Download mandatory content only
         
-        "/usr/local/bin/appleLoops" --deployment --mandatory-only
+        "/usr/local/bin/appleLoops" --apps garageband --deployment --mandatory-only
         
         # Download optional content only if argument == optional
         
         if [ "${1}" == "optional" ]; then
-          "/usr/local/bin/appleLoops" --deployment --optional-only
+          "/usr/local/bin/appleLoops" --apps garageband --deployment --optional-only
         fi
         
       else
