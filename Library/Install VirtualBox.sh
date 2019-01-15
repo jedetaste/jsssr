2d2e8dfbb75c39701eb8853706c53eedb94ce976
Anver Housseini <contact@anver.ch>
2019-01-15 20:34:08 +0100

    Create Install VirtualBox.sh

---
+++ Library/Install VirtualBox.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+  /usr/local/bin/aky virtualbox
+  
+  SET_UID_BINARIES="VBoxNetAdpCtl"
+  SET_UID_BINARIES="${SET_UID_BINARIES} VBoxHeadless VirtualBoxVM VBoxNetDHCP VBoxNetNAT" # WITH_HARDENING
+  for bin in ${SET_UID_BINARIES}; do
+    chmod u+s "/Applications/VirtualBox.app/Contents/MacOS/${bin}"
+  done
