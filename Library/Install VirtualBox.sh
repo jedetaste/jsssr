5d5078b01c66b71f1f5a221a6576a878237de406
Anver Housseini <contact@anver.ch>
2019-01-15 20:36:00 +0100

    Update Install VirtualBox.sh

--- Library/Install VirtualBox.sh
+++ Library/Install VirtualBox.sh
@@ -5,5 +5,6 @@
   SET_UID_BINARIES="VBoxNetAdpCtl"
   SET_UID_BINARIES="${SET_UID_BINARIES} VBoxHeadless VirtualBoxVM VBoxNetDHCP VBoxNetNAT"
   for bin in ${SET_UID_BINARIES}; do
+    echo "==> Adjust permissions for '/Applications/VirtualBox.app/Contents/MacOS/${bin}'"
     chmod u+s "/Applications/VirtualBox.app/Contents/MacOS/${bin}"
   done
