f26aa6568ea18a075f017bc299ae1e4c23ed0a02
Anver Housseini <contact@anver.ch>
2019-01-15 10:13:07 +0100

    Delete Install VirtualBox.sh

--- Library/Install VirtualBox.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-	/usr/local/bin/aky virtualbox
-	
-	for bin in VirtualBox VirtualBoxVM VBoxNetAdpCtl VBoxNetDHCP VBoxNetNAT VBoxHeadless; do
-		echo "==> Adjust permissions for '/Applications/VirtualBox.app/Contents/MacOS/${bin}'"
-		/bin/chmod u+s "/Applications/VirtualBox.app/Contents/MacOS/${bin}"
-	done
