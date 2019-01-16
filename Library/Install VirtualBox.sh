5c7b99eda5eda8fd4191976d3e7cd493b68d22c4
Anver Housseini <contact@anver.ch>
2019-01-16 09:10:37 +0100

    Update Install VirtualBox.sh

--- Library/Install VirtualBox.sh
+++ Library/Install VirtualBox.sh
@@ -8,3 +8,17 @@
     echo "==> Adjust permissions for '/Applications/VirtualBox.app/Contents/MacOS/${bin}'"
     chmod u+s "/Applications/VirtualBox.app/Contents/MacOS/${bin}"
   done
+  
+  version=$( curl --silent "https://download.virtualbox.org/virtualbox/LATEST.TXT" )
+  echo "==> Install 'VirtualBox ${version} Oracle VM VirtualBox Extension Pack'"
+  
+  curl \
+    --silent \
+    --output "/private/tmp/Oracle_VM_VirtualBox_Extension_Pack-${version}.vbox-extpack" \
+    "https://download.virtualbox.org/virtualbox/${version}/Oracle_VM_VirtualBox_Extension_Pack-${version}.vbox-extpack"
+  
+  echo "y" | /usr/local/bin/VBoxManage extpack install --replace "/private/tmp/Oracle_VM_VirtualBox_Extension_Pack-${version}.vbox-extpack"
+  
+  if [ -s "/private/tmp/Oracle_VM_VirtualBox_Extension_Pack-${version}.vbox-extpack" ]; then
+    rm -rf "/private/tmp/Oracle_VM_VirtualBox_Extension_Pack-${version}.vbox-extpack"
+  fi
