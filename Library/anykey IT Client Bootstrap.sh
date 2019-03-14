483c7d311e6d858d7de9762d8e2cbc9f28af7bce
ahousseini <admin@anver-2.local>
2019-03-14 18:38:45 +0100

    Update anykey IT Client Bootstrap.sh

--- Library/anykey IT Client Bootstrap.sh
+++ Library/anykey IT Client Bootstrap.sh
@@ -11,16 +11,16 @@
   macos_vers
   
   if [ ${minor} -gt 11 ]; then
-    echo "==> macOS ${major}.${minor}.${revision}"
+    echo "==> Computer is running macOS ${major}.${minor}.${revision}"
   else
-    echo "==> OS X ${major}.${minor}.${revision}"
+    echo "==> Computer is running OS X ${major}.${minor}.${revision}"
   fi
   
   # Symlink useful applications
   
   echo "==> Symlink useful applications"
   
-  echo "    Symlink 'Directory Utility.app'"
+  echo "Symlink 'Directory Utility.app'"
   if [[ ! -e "/Applications/Utilities/Directory Utility.app" ]]; then
     ln -s "/System/Library/CoreServices/Applications/Directory Utility.app" "/Applications/Utilities/Directory Utility.app"
   fi
@@ -30,7 +30,7 @@
     ln -s "/System/Library/CoreServices/Applications/Directory Utility.app" "/Applications/Utilities/Directory Utility.app"
   fi
   
-  echo "    Symlink 'Network Utility.app'"
+  echo "Symlink 'Network Utility.app'"
   if [[ ! -e "/Applications/Utilities/Network Utility.app" ]]; then
     ln -s "/System/Library/CoreServices/Applications/Network Utility.app" "/Applications/Utilities/Network Utility.app"
   fi
@@ -40,7 +40,7 @@
     ln -s "/System/Library/CoreServices/Applications/Network Utility.app" "/Applications/Utilities/Network Utility.app"
   fi
   
-  echo "    Symlink 'Screen Sharing.app'"
+  echo "Symlink 'Screen Sharing.app'"
   if [[ ! -e "/Applications/Utilities/Screen Sharing.app" ]]; then
     ln -s "/System/Library/CoreServices/Applications/Screen Sharing.app" "/Applications/Utilities/Screen Sharing.app"
   fi
@@ -50,7 +50,7 @@
     ln -s "/System/Library/CoreServices/Applications/Screen Sharing.app" "/Applications/Utilities/Screen Sharing.app"
   fi
   
-  echo "    Symlink 'jamf.log'"
+  echo "Symlink 'jamf.log'"
   if [[ ! -e "/Users/admin/Desktop/jamf.log" ]]; then
     ln -s "/var/log/jamf.log" "/Users/admin/Desktop/jamf.log"
   fi
@@ -93,11 +93,9 @@
   
   # Make mach_kernel invisible
   
-  echo "==> Make '/mach_kernel' invisible"
-  
   if [ -e "/mach_kernel" ]; then
     if ! ls -lO /mach_kernel | grep hidden > /dev/null; then
-      echo "/mach_kernel not set to be hidden. Re-hiding."
+      echo "==> /mach_kernel not set to be hidden. Re-hiding."
       chflags hidden "/mach_kernel"
     fi
   fi
@@ -166,13 +164,13 @@
   
   # Disable Safari setup assistent in all User Template
   
-  echo "==> Disable Safari setup assistent in all User Template"
+  echo "==> Disable Safari setup assistent in all user template"
   
   safariversion=$(defaults read /Applications/Safari.app/Contents/Info CFBundleShortVersionString | /usr/bin/awk '{print $1}')
   
-  for USER_TEMPLATE in "/System/Library/User Template"/*; do
-    defaults write "${USER_TEMPLATE}/Library/Preferences/com.apple.Safari" LastOSVersionSafariWasLaunchedOn -string "${minor}"
-    defaults write "${USER_TEMPLATE}/Library/Preferences/com.apple.Safari" LastSafariVersionWithWelcomePage -string "${safariversion}"
+  for user_template in "/System/Library/User Template"/*; do
+    defaults write "${user_template}/Library/Preferences/com.apple.Safari" LastOSVersionSafariWasLaunchedOn -string "${minor}"
+    defaults write "${user_template}/Library/Preferences/com.apple.Safari" LastSafariVersionWithWelcomePage -string "${safariversion}"
   done
   
   # Disable Oracle Java Auto Update
@@ -192,47 +190,28 @@
   
   # Disable Auto Update Adobe Acrobat Reader DC
   
-  echo "==> Disable Auto Update Adobe Acrobat Reader DC"
-  
   if [ -d "/Applications/Adobe Acrobat Reader DC.app/Contents/Plugins/Updater.acroplugin" ]; then
+    echo "==> Disable Auto Update Adobe Acrobat Reader DC"
     rm -rf "/Applications/Adobe Acrobat Reader DC.app/Contents/Plugins/Updater.acroplugin"
   fi
   
   # Delete Adobe Reader Plugins
   
-  echo "==> Delete Adobe Reader Plugins"
-  
   if [ -e "/Library/Internet Plug-Ins/AdobePDFViewer.plugin" ]; then
+    echo "==> Delete Adobe Reader Plugins"
     rm -rf "/Library/Internet Plug-Ins/AdobePDFViewer.plugin"
   fi
   
   if [ -e "/Library/Internet Plug-Ins/AdobePDFViewerNPAPI.plugin" ]; then
+    echo "==> Delete Adobe Reader Plugins"
     rm -rf "/Library/Internet Plug-Ins/AdobePDFViewerNPAPI.plugin"
-  fi
-  
-  # Disable Spotify AutoStart if installed
-  
-  echo "==> Disable Spotify AutoStart if installed"
-  
-  if [ -e "/Applications/Spotify.app" ]; then
-    for usertemplate in "/System/Library/User Template"/*; do
-      if [ ! -s "${usertemplate}/Library/Application Support/Spotify/prefs" ]; then
-        mkdir -p "${usertemplate}/Library/Application Support/Spotify/"
-        echo "app.autostart-configured=false" >> "${usertemplate}/Library/Application Support/Spotify/prefs"
-      else
-        rm -f "${usertemplate}/Library/Application Support/Spotify/prefs"
-        mkdir -p "${usertemplate}/Library/Application Support/Spotify/"
-        echo "app.autostart-configured=false" >> "${usertemplate}/Library/Application Support/Spotify/prefs"
-      fi
-    done
   fi
 
   # Correct Permissions Adobe Folder
   
-  echo "==> Correct Permissions Adobe Folder"
-  
   for user in $(ls /Users | grep -v Shared | grep -v Guest | grep -v '.localized'); do
     if [ -d "/Users/${user}/Library/Application Support/Adobe/" ];  then
+      echo "==> Correct Permissions Adobe Folder"
       chown -R ${user} "/Users/${user}/Library/Application Support/Adobe/"
       chmod -R 700 "/Users/${user}/Library/Application Support/Adobe/"
     fi
@@ -240,8 +219,7 @@
   
   # Remove wrongly saved Remove2011.log
   
-  echo "==> Remove wrongly saved Remove2011.log"
-  
   if [ -s "/Remove2011.log" ]; then
+    echo "==> Remove wrongly saved Remove2011.log"
     rm -f "/Remove2011.log"
   fi
