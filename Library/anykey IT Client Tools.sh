8090d33d9a8e682ecedce369837461313b9b8833
ahousseini <admin@admin-3.local>
2019-03-25 14:47:29 +0100

    Update anykey IT Client Tools.sh

--- Library/anykey IT Client Tools.sh
+++ Library/anykey IT Client Tools.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-  
+
   # Determine macOS Version
-  
+
   macos_vers() {
     IFS='.' read -r major minor revision < <(/usr/bin/sw_vers -productVersion)
   }
-  
+
   macos_vers
-  
+
   if [ ${minor} -gt 11 ]; then
     echo "==> macOS version is '${major}.${minor}.${revision}'"
   else
     echo "==> OS X version is '${major}.${minor}.${revision}'"
   fi
-  
+
   # appleLoops
-  
+
   echo "==> Install 'appleLoops'"
-  
+
   if [ -s "/usr/local/bin/appleLoops" ]; then
     rm -f "/usr/local/bin/appleLoops"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/appleLoops" "https://raw.githubusercontent.com/carlashley/appleLoops/master/appleLoops.py"
 
   /usr/sbin/chown root:wheel "/usr/local/bin/appleLoops"
@@ -29,125 +29,125 @@
   /bin/chmod +x "/usr/local/bin/appleLoops"
 
   # dockutil
-  
+
   echo "==> Install 'dockutil'"
-  
+
   if [ -s "/usr/local/bin/dockutil" ]; then
     rm -f "/usr/local/bin/dockutil"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/dockutil" "https://raw.githubusercontent.com/kcrawford/dockutil/master/scripts/dockutil"
 
   /usr/sbin/chown root:wheel "/usr/local/bin/dockutil"
   /bin/chmod 775 "/usr/local/bin/dockutil"
   /bin/chmod +x "/usr/local/bin/dockutil"
-  
+
   # currentuser
-  
+
   echo "==> Install 'currentuser'"
-  
+
   if [ -s "/usr/local/bin/currentuser" ]; then
     rm -f "/usr/local/bin/currentuser"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/currentuser" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/currentuser"
-  
+
   /usr/sbin/chown root:wheel "/usr/local/bin/currentuser"
   /bin/chmod 775 "/usr/local/bin/currentuser"
   /bin/chmod +x "/usr/local/bin/currentuser"
-  
+
   # tmpDir
-  
+
   echo "==> Install 'tmpDir'"
-  
+
   if [ -s "/usr/local/bin/tmpDir" ]; then
     rm -f "/usr/local/bin/tmpDir"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/tmpDir" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/tmpDir"
-  
+
   /usr/sbin/chown root:wheel "/usr/local/bin/tmpDir"
   /bin/chmod 775 "/usr/local/bin/tmpDir"
   /bin/chmod +x "/usr/local/bin/tmpDir"
-  
+
   # csc
-  
+
   echo "==> Install 'csc'"
-  
+
   if [ -s "/usr/local/bin/csc" ]; then
     rm -f "/usr/local/bin/csc"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/csc" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/csc"
-  
+
   /usr/sbin/chown root:wheel "/usr/local/bin/csc"
   /bin/chmod 775 "/usr/local/bin/csc"
   /bin/chmod +x "/usr/local/bin/csc"
-  
+
   # defaultbrowser
-  
+
   echo "==> Install 'defaultbrowser'"
-  
+
   if [ -s "/usr/local/bin/defaultbrowser" ]; then
     rm -f "/usr/local/bin/defaultbrowser"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/defaultbrowser" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/defaultbrowser"
-  
+
   /usr/sbin/chown root:wheel "/usr/local/bin/defaultbrowser"
   /bin/chmod 775 "/usr/local/bin/defaultbrowser"
   /bin/chmod +x "/usr/local/bin/defaultbrowser"
-  
+
   # mysides
-  
+
   echo "==> Install 'mysides'"
-  
+
   if [ -s "/usr/local/bin/mysides" ]; then
     rm -f "/usr/local/bin/mysides"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/mysides" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/mysides"
-  
+
   /usr/sbin/chown root:wheel "/usr/local/bin/mysides"
   /bin/chmod 775 "/usr/local/bin/mysides"
   /bin/chmod +x "/usr/local/bin/mysides"
-  
+
   # pkgfixer
-  
+
   echo "==> Install 'pkgfixer'"
-  
+
   if [ -s "/usr/local/bin/pkgfixer" ]; then
     rm -f "/usr/local/bin/pkgfixer"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/pkgfixer" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/pkgfixer"
-  
+
   /usr/sbin/chown root:wheel "/usr/local/bin/pkgfixer"
   /bin/chmod 775 "/usr/local/bin/pkgfixer"
   /bin/chmod +x "/usr/local/bin/pkgfixer"
-  
+
   # blueutil
-  
+
   echo "==> Install 'blueutil'"
-  
+
   if [ -s "/usr/local/bin/blueutil" ]; then
     rm -f "/usr/local/bin/blueutil"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/blueutil" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/blueutil"
-  
+
   /usr/sbin/chown root:wheel "/usr/local/bin/blueutil"
   /bin/chmod 775 "/usr/local/bin/blueutil"
   /bin/chmod +x "/usr/local/bin/blueutil"
-  
-  # aky
-  
-  echo "==> Install 'aky'"
-  
+
+  # Purge legacy aky
+
+  echo "==> Purge legacy 'aky'"
+
   if [ -d "/usr/local/aky/" ]; then
     rm -rf "/usr/local/aky/"
   fi
-  
+
   aky_binary=(
     "aky"
     "aria2c@el_capitan"
@@ -162,23 +162,25 @@
     "rg"
     "versioning-helper"
   )
-  
+
   for ((i = 0; i < "${#aky_binary[@]}"; i++)); do
-    
+
     [ -L "/usr/local/bin/${aky_binary[$i]}" ] && unlink "/usr/local/bin/${aky_binary[$i]}"
-    
+
     if [ -s "/usr/local/bin/${aky_binary[$i]}" ]; then
       rm -f "/usr/local/bin/${aky_binary[$i]}"
     fi
-    
+
   done
-  
-  # Install aky binaries
-  
+
+  # Install aky
+
+  echo "==> Install 'aky'"
+
   if [ ! -d "/usr/local/bin" ]; then
     /bin/mkdir -p "/usr/local/bin"
   fi
-  
+
   aky_binary=(
     "aky"
     "rg"
@@ -187,321 +189,321 @@
     "aria2c"
     "tmpDir"
   )
-  
+
   for ((i = 0; i < "${#aky_binary[@]}"; i++)); do
     /bin/rm -f "/usr/local/bin/${aky_binary[$i]}"
     /usr/bin/curl -so "/usr/local/bin/${aky_binary[$i]}" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/aky-src/${aky_binary[$i]}"
     /usr/sbin/chown root:wheel "/usr/local/bin/${aky_binary[$i]}"
     /bin/chmod +x "/usr/local/bin/${aky_binary[$i]}"
   done
-  
+
   # Remove2011
-  
+
   echo "==> Install 'Remove2011'"
-  
+
   if [ -s "/usr/local/bin/Remove2011" ]; then
     rm -f "/usr/local/bin/Remove2011"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/Remove2011" "https://raw.githubusercontent.com/pbowden-msft/Remove2011/master/Remove2011"
-  
+
   /usr/sbin/chown root:wheel "/usr/local/bin/Remove2011"
   /bin/chmod 775 "/usr/local/bin/Remove2011"
   /bin/chmod +x "/usr/local/bin/Remove2011"
-  
+
   # adobe_prtk
-  
+
   echo "==> Install 'adobe_prtk'"
-  
+
   if [ -s "/usr/local/bin/adobe_prtk" ]; then
     rm -f "/usr/local/bin/adobe_prtk"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/adobe_prtk" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/adobe_prtk"
-  
+
   /usr/sbin/chown root:wheel "/usr/local/bin/adobe_prtk"
   /bin/chmod 775 "/usr/local/bin/adobe_prtk"
   /bin/chmod +x "/usr/local/bin/adobe_prtk"
-  
+
   # AppStoreXtractor
-  
+
   echo "==> Install 'AppStoreXtractor'"
-  
+
   if [ -s "/usr/local/bin/AppStoreXtractor" ]; then
     rm -f "/usr/local/bin/AppStoreXtractor"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/AppStoreXtractor" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/AppStoreXtractor"
-  
+
   /usr/sbin/chown root:wheel "/usr/local/bin/AppStoreXtractor"
   /bin/chmod 775 "/usr/local/bin/AppStoreXtractor"
   /bin/chmod +x "/usr/local/bin/AppStoreXtractor"
-  
+
   # outset
-  
+
   echo "==> Install 'outset'"
-  
+
   outsetVersion="2.0.6"
-  
+
   tmpFolder=$(getconf DARWIN_USER_CACHE_DIR) && randString=$(/usr/bin/openssl rand -hex 5) && tmpDir="${tmpFolder}${randString}" && /bin/mkdir -p "${tmpDir}"
-  
+
   cd "${tmpDir}" && /usr/bin/curl -s -O -J -L "https://github.com/chilcote/outset/releases/download/v${outsetVersion}/outset-${outsetVersion}.pkg"
-  
+
   /usr/sbin/installer -pkg "${tmpDir}/outset-${outsetVersion}.pkg" -target / > /dev/null 2>&1 && rm -rf "${tmpDir}"
-  
+
   # offset
-  
+
   echo "==> Install 'offset'"
-  
+
   offsetVersion="1.4.1"
-  
+
   tmpFolder=$(getconf DARWIN_USER_CACHE_DIR) && randString=$(/usr/bin/openssl rand -hex 5) && tmpDir="${tmpFolder}${randString}" && /bin/mkdir -p "${tmpDir}"
-  
+
   cd "${tmpDir}" && /usr/bin/curl -s -O -J -L "https://github.com/aysiu/offset/releases/download/${offsetVersion}/Offset.pkg"
-  
+
   /usr/sbin/installer -pkg "${tmpDir}/Offset.pkg" -target / > /dev/null 2>&1 && rm -rf "${tmpDir}"
-  
+
   # SafariBookmarkEditor
-  
+
   echo "==> Install 'SafariBookmarkEditor'"
-  
+
   if [ -s "/usr/local/bin/SafariBookmarkEditor" ]; then
     rm -f "/usr/local/bin/SafariBookmarkEditor"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/SafariBookmarkEditor" "https://raw.githubusercontent.com/robperc/SafariBookmarkEditor/master/SafariBookmarkEditor.py"
-  
+
   /usr/sbin/chown root:wheel "/usr/local/bin/SafariBookmarkEditor"
   /bin/chmod 775 "/usr/local/bin/SafariBookmarkEditor"
   /bin/chmod +x "/usr/local/bin/SafariBookmarkEditor"
-  
+
   # FinderSidebarEditor
-  
+
   echo "==> Install 'FinderSidebarEditor'"
-  
+
   if [ -s "/usr/local/bin/FinderSidebarEditor" ]; then
     rm -f "/usr/local/bin/FinderSidebarEditor"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/FinderSidebarEditor" "https://raw.githubusercontent.com/robperc/FinderSidebarEditor/master/FinderSidebarEditor.py"
-  
+
   /usr/sbin/chown root:wheel "/usr/local/bin/FinderSidebarEditor"
   /bin/chmod 775 "/usr/local/bin/FinderSidebarEditor"
   /bin/chmod +x "/usr/local/bin/FinderSidebarEditor"
-  
+
   # duti
-  
+
   echo "==> Install 'duti'"
-  
+
   if [ -s "/usr/local/bin/duti" ]; then
     rm -f "/usr/local/bin/duti"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/duti" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/duti"
-  
+
   /usr/sbin/chown root:wheel "/usr/local/bin/duti"
   /bin/chmod 775 "/usr/local/bin/duti"
   /bin/chmod +x "/usr/local/bin/duti"
-  
+
   # ncdu
-  
+
   echo "==> Install 'ncdu'"
-  
+
   if [ -s "/usr/local/bin/ncdu" ]; then
     rm -f "/usr/local/bin/ncdu"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/ncdu" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/ncdu"
-  
+
   /usr/sbin/chown root:wheel "/usr/local/bin/ncdu"
   /bin/chmod 775 "/usr/local/bin/ncdu"
   /bin/chmod +x "/usr/local/bin/ncdu"
-  
+
   # mas
-  
+
   if [ ${minor} -ge 13 ]; then
-    
+
     echo "==> Install 'mas'"
-    
+
     masVersion="1.6.2"
-    
+
     tmpFolder=$(getconf DARWIN_USER_CACHE_DIR) && randString=$(/usr/bin/openssl rand -hex 5) && tmpDir="${tmpFolder}${randString}" && /bin/mkdir -p "${tmpDir}"
-    
+
     cd "${tmpDir}" && /usr/bin/curl -s -O -J -L "https://github.com/mas-cli/mas/releases/download/v1.6.1/mas.pkg"
-    
+
     /usr/sbin/installer -pkg "${tmpDir}/mas.pkg" -target / > /dev/null 2>&1 && rm -rf "${tmpDir}"
-    
+
   fi
-  
+
   # xmlstarlet
-  
+
   echo "==> Install 'xmlstarlet'"
-  
+
   if [ -s "/usr/local/bin/xmlstarlet" ]; then
     rm -f "/usr/local/bin/xmlstarlet"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/xmlstarlet" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/xmlstarlet"
-  
+
   /usr/sbin/chown root:wheel "/usr/local/bin/xmlstarlet"
   /bin/chmod 775 "/usr/local/bin/xmlstarlet"
   /bin/chmod +x "/usr/local/bin/xmlstarlet"
-  
+
   # reattach-to-user-namespace
-  
+
   echo "==> Install 'reattach-to-user-namespace'"
-  
+
   if [ -s "/usr/local/bin/reattach-to-user-namespace" ]; then
     rm -f "/usr/local/bin/reattach-to-user-namespace"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/reattach-to-user-namespace" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/reattach-to-user-namespace"
-  
+
   /usr/sbin/chown root:wheel "/usr/local/bin/reattach-to-user-namespace"
   /bin/chmod 775 "/usr/local/bin/reattach-to-user-namespace"
   /bin/chmod +x "/usr/local/bin/reattach-to-user-namespace"
-  
+
   # RegMAU
-  
+
   echo "==> Install 'RegMAU'"
-  
+
   if [ -s "/usr/local/bin/RegMAU" ]; then
     rm -f "/usr/local/bin/RegMAU"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/RegMAU" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/RegMAU"
-  
+
   /usr/sbin/chown root:wheel "/usr/local/bin/RegMAU"
   /bin/chmod 775 "/usr/local/bin/RegMAU"
   /bin/chmod +x "/usr/local/bin/RegMAU"
-  
+
   # MSUpdateHelper
 
   if [ -s "/usr/local/bin/MSUpdateHelper2019" ]; then
     rm -f "/usr/local/bin/MSUpdateHelper2019"
   fi
-  
+
   echo "==> Install 'MSUpdateHelper'"
-  
+
   if [ -s "/usr/local/bin/MSUpdateHelper" ]; then
     rm -f "/usr/local/bin/MSUpdateHelper"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/MSUpdateHelper" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/MSUpdateHelper"
-  
+
   /usr/sbin/chown root:wheel "/usr/local/bin/MSUpdateHelper"
   /bin/chmod 775 "/usr/local/bin/MSUpdateHelper"
   /bin/chmod +x "/usr/local/bin/MSUpdateHelper"
-  
+
   # alerter
-  
+
   echo "==> Install 'alerter'"
-  
+
   if [ -s "/usr/local/bin/alerter" ]; then
     rm -f "/usr/local/bin/alerter"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/alerter" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/alerter"
-  
+
   /usr/sbin/chown root:wheel "/usr/local/bin/alerter"
   /bin/chmod 775 "/usr/local/bin/alerter"
   /bin/chmod +x "/usr/local/bin/alerter"
-  
+
   # PrinterMapper
-  
+
   echo "==> Install 'PrinterMapper'"
-  
+
   if [ -s "/usr/local/bin/PrinterMapper" ]; then
     rm -f "/usr/local/bin/PrinterMapper"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/PrinterMapper" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/PrinterMapper"
-  
+
   /usr/sbin/chown root:wheel "/usr/local/bin/PrinterMapper"
   /bin/chmod 775 "/usr/local/bin/PrinterMapper"
   /bin/chmod +x "/usr/local/bin/PrinterMapper"
-  
+
   # lohelper
-  
+
   echo "==> Install 'lohelper'"
-  
+
   if [ -s "/usr/local/bin/lohelper" ]; then
     rm -f "/usr/local/bin/lohelper"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/lohelper" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/lohelper"
-  
+
   /usr/sbin/chown root:wheel "/usr/local/bin/lohelper"
   /bin/chmod 775 "/usr/local/bin/lohelper"
   /bin/chmod +x "/usr/local/bin/lohelper"
-  
+
   # randomizer
-  
+
   echo "==> Install 'randomizer'"
-  
+
   if [ -s "/usr/local/bin/randomizer" ]; then
     rm -f "/usr/local/bin/randomizer"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/randomizer" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/randomizer"
-  
+
   /usr/sbin/chown root:wheel "/usr/local/bin/randomizer"
   /bin/chmod 775 "/usr/local/bin/randomizer"
   /bin/chmod +x "/usr/local/bin/randomizer"
-  
+
   # installinstallmacos
-  
+
   if [ -s "/usr/local/bin/installinstallmacos" ]; then
     echo "==> Uninstall 'installinstallmacos'"
     rm -f "/usr/local/bin/installinstallmacos"
   fi
-  
+
   # assimilateownership
-  
+
   echo "==> Install 'assimilateownership'"
-  
+
   if [ -s "/usr/local/bin/assimilateownership" ]; then
     rm -f "/usr/local/bin/assimilateownership"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/assimilateownership" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/assimilateownership"
-  
+
   /usr/sbin/chown root:wheel "/usr/local/bin/assimilateownership"
   /bin/chmod 775 "/usr/local/bin/assimilateownership"
   /bin/chmod +x "/usr/local/bin/assimilateownership"
-  
+
   # erase-install
-  
+
   echo "==> Install 'erase-install'"
-  
+
   if [ -s "/usr/local/bin/erase-install" ]; then
     rm -f "/usr/local/bin/erase-install"
   fi
-  
+
   /usr/bin/curl -so "/usr/local/bin/erase-install" "https://raw.githubusercontent.com/grahampugh/erase-install/master/erase-install.sh"
-  
+
   /usr/sbin/chown root:wheel "/usr/local/bin/erase-install"
   /bin/chmod 775 "/usr/local/bin/erase-install"
   /bin/chmod +x "/usr/local/bin/erase-install"
-  
+
   # jpscheck
-  
+
   echo "==> Install 'jpscheck'"
-  
+
   if [ -s "/Library/LaunchDaemons/ch.anykey.jpscheck.plist" ]; then
     /bin/launchctl unload -w "/Library/LaunchDaemons/ch.anykey.jpscheck.plist"
     rm -rf "/Library/LaunchDaemons/ch.anykey.jpscheck.plist"
   fi
-  
+
   /usr/local/bin/jamf scheduledTask \
     -command "/usr/local/bin/jpscheck" \
     -name "jpscheck" \
     -user "root" \
     -runAtLoad "true" \
     -minute "*/30/"
-    
+
   /usr/bin/curl -so "/usr/local/bin/jpscheck" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/jpscheck"
-  
+
   /usr/sbin/chown root:wheel "/usr/local/bin/jpscheck"
   /bin/chmod 775 "/usr/local/bin/jpscheck"
   /bin/chmod +x "/usr/local/bin/jpscheck"
