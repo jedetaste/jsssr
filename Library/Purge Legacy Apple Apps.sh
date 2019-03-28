28cba6924bcf342983f122d4ffc5d30465129ec7
ahousseini <admin@anver-mac.local>
2019-03-28 10:35:29 +0100

    Update Purge Legacy Apple Apps.sh

--- Library/Purge Legacy Apple Apps.sh
+++ Library/Purge Legacy Apple Apps.sh
@@ -11,7 +11,10 @@
   for applications in "${applications_list[@]}"; do
     if [ -s "/Applications/${applications}" ]; then
       if [ $(mdls -raw -name kMDItemAppStoreHasReceipt /Applications/${applications}) == "(null)" ]; then
+        echo "==> Remove legacy '/Applications/${applications}'"
         rm -rf "/Applications/${applications}"
+      else
+        echo "==> Application '/Applications/${applications}' has already an App Store receipt"
       fi
     fi
   done
