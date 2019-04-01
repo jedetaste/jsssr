116e441f4061992894c00953d1781867abdb7bb2
ahousseini <admin@anver-mac.local>
2019-04-01 15:07:18 +0200

    Update Install macOS Mojave.sh

--- Library/Install macOS Mojave.sh
+++ Library/Install macOS Mojave.sh
@@ -20,12 +20,17 @@
   if [ ! -s "${installer}" ]; then
     if [ ! -z "${current_user}" ] && finder_running; then
       if [ -s "${installer}" ]; then
+
         if [[ ${user_language} = en* ]]; then
           "${jamf_helper}" -windowType fs -title "macOS ${installer_name} Upgrade" -alignHeading center -heading "Please wait as we prepare your computer for macOS ${installer_name}" -alignDescription center -description "This process will take approximately 5-10 minutes. Once completed your computer will reboot and begin the upgrade." -icon "${installer}/Contents/Resources/InstallAssistant.icns" &
         elif [[ ${user_language} = de* ]]; then
           "${jamf_helper}" -windowType fs -title "macOS ${installer_name} Upgrade " -alignHeading center -heading "Bitte warten, das Upgrade macOS ${installer_name} wird ausgeführt" -alignDescription center -description "Dieser Prozess benötigt ungefähr 5-10 Minuten. Der Mac startet anschliessend neu und beginnt mit dem Update." -icon "${installer}/Contents/Resources/InstallAssistant.icns" &
         fi
+
+        curl -s -o "/tmp/First_Boot_Recon.pkg" "https://cdn-clients.anykeyit.ch/Static/First_Boot_Recon.pkg" && installer -pkg "/tmp/First_Boot_Recon.pkg" -target /
+
         "${installer}/Contents/Resources/startosinstall" --agreetolicense --nointeraction --pidtosignal ${jamf_helper_pid} &
+
       fi
     else
       echo "User is not logged in since the '${installer}/Contents/Resources/startosinstall' binary requires a user to be logged in." && exit 1
