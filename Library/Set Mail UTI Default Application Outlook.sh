798767c92f95c508511d16abde66dbbe78439759
ahousseini <admin@anver-mac.intra.anver.ch>
2019-07-04 15:00:16 +0200

    Create Set Mail UTI Default Application Outlook.sh

---
+++ Library/Set Mail UTI Default Application Outlook.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+  su -l $(/usr/local/bin/currentuser) -c "/usr/local/bin/duti -s com.microsoft.outlook mailto"
+  su -l $(/usr/local/bin/currentuser) -c "/usr/local/bin/duti -s com.microsoft.outlook ics all"
+  su -l $(/usr/local/bin/currentuser) -c "/usr/local/bin/duti -s com.microsoft.outlook vcf all"
