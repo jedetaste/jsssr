3b80df60c58d094092d0922efc2c4a5ac033af30
ahousseini <admin@anver-mac.intra.anver.ch>
2019-07-09 13:34:38 +0200

    Create Make Dock Non-Immutable.sh

---
+++ Library/Make Dock Non-Immutable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+currentuser=$(/usr/local/bin/currentuser)
+
+if [ -n "${currentuser}" ]; then
+	if [ "$(su -l "${currentuser}" -c "defaults read com.apple.dock contents-immutable 2> /dev/null")" -eq 1 ]; then
+		su -l "${currentuser}" -c "defaults write com.apple.dock contents-immutable -bool false"
+		su -l "${currentuser}" -c "killall Dock"
+	fi
+fi
