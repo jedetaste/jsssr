61dc7b947c77ef02b1ee6b800d94a020be011cf5
ahousseini <admin@anver-mac.intra.anver.ch>
2019-07-09 14:17:38 +0200

    Update Install SchoolCraftPremiumContent.sh

--- Library/Install SchoolCraftPremiumContent.sh
+++ Library/Install SchoolCraftPremiumContent.sh
@@ -1,36 +1,40 @@
 #!/bin/bash
 
+  version="2019.2.4.10"
+  download="https://www.hidrive.strato.com/wget/TpAqjsrr"
+
+  if [ -s "/Library/Application Support/WorksheetCrafter/SchoolCraft Premium Content.bundle" ]; then
+    version_installed=$(cat "/Library/Application Support/WorksheetCrafter/SchoolCraft Premium Content.bundle/version.txt" | tr -d '[:space:]')
+    if [ "${version_installed}" = "${version}" ]; then
+      echo "==> School Craft Premium Content is up-to-date with v${version}" && exit 0
+    fi
+  fi
+
   tmpDir=$(/usr/local/bin/tmpDir)
 
-  echo "==> Download 'https://www.hidrive.strato.com/wget/TpAqjsrr'"
-  curl -s -o "${tmpDir}/SchoolCraftPremiumContent.pkg" "https://www.hidrive.strato.com/wget/TpAqjsrr"
+  echo "==> Download '${download}'"
+  curl -s -o "${tmpDir}/SchoolCraftPremiumContent.pkg" "${download}"
 
   if [ -s "${tmpDir}/SchoolCraftPremiumContent.pkg" ]; then
 
     echo "==> Prepare '${tmpDir}/SchoolCraftPremiumContent.pkg'"
-    pkgutil --expand "${tmpDir}/SchoolCraftPremiumContent.pkg" "${tmpDir}/SchoolCraftPremiumContent-expanded.pkg"
+    pkgutil --expand-full "${tmpDir}/SchoolCraftPremiumContent.pkg" "${tmpDir}/SchoolCraftPremiumContent-expanded.pkg"
 
-    cat > "${tmpDir}/SchoolCraftPremiumContent-expanded.pkg/SchoolCraftPremiumContent.pkg/Scripts/postinstall" <<EOF
-#!/bin/sh
-exit 0
-EOF
-
-    pkgutil --flatten "${tmpDir}/SchoolCraftPremiumContent-expanded.pkg" "${tmpDir}/SchoolCraftPremiumContent-flatten.pkg"
-
-    echo "==> Install '${tmpDir}/SchoolCraftPremiumContent-flatten.pkg'"
-    installer -pkg "${tmpDir}/SchoolCraftPremiumContent-flatten.pkg" -target /
-
-    mkdir -p "/Library/Application Support/WorksheetCrafter"
-
+    echo "==> Extract Payload 'SchoolCraft Premium Content.bundle'"
     if [ -s "/Library/Application Support/WorksheetCrafter/SchoolCraft Premium Content.bundle" ]; then
       rm -rf "/Library/Application Support/WorksheetCrafter/SchoolCraft Premium Content.bundle"
     fi
 
-    mv "/private/tmp/SchoolCraft Premium Content.bundle" "/Library/Application Support/WorksheetCrafter/SchoolCraft Premium Content.bundle"
+    mkdir -p "/Library/Application Support/WorksheetCrafter"
 
+    mv "${tmpDir}/SchoolCraftPremiumContent-expanded.pkg/SchoolCraftPremiumContent.pkg/Payload/tmp/SchoolCraft Premium Content.bundle" "/Library/Application Support/WorksheetCrafter/SchoolCraft Premium Content.bundle"
+    chmod -R 755 "/Library/Application Support/WorksheetCrafter/SchoolCraft Premium Content.bundle"
+    
     for user in $(ls /Users | grep -v Shared | grep -v Guest | grep -v '.localized'); do
       if [ -s "/Users/${user}/Pictures/SchoolCraft Premium Content.bundle" ]; then
         rm -rf "/Users/${user}/Pictures/SchoolCraft Premium Content.bundle"
+        echo "==> Symlink '/Users/${user}/Pictures/SchoolCraft Premium Content.bundle'"
+        ln -s "/Library/Application Support/WorksheetCrafter/SchoolCraft Premium Content.bundle" "/Users/${user}/Pictures"
       elif [ ! -s "/Users/${user}/Pictures/SchoolCraft Premium Content.bundle" ]; then
         echo "==> Symlink '/Users/${user}/Pictures/SchoolCraft Premium Content.bundle'"
         ln -s "/Library/Application Support/WorksheetCrafter/SchoolCraft Premium Content.bundle" "/Users/${user}/Pictures"
@@ -40,3 +44,4 @@ EOF
   fi
 
   rm -rf "${tmpDir}"
+
