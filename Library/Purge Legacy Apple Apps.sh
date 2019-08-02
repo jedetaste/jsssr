bce4c64dcbe9f9658bb7a7341fcb88965bfd6542
ahousseini <contact@anver.ch>
2019-08-02 14:07:58 +0200

    Update Purge Legacy Apple Apps.sh

--- Library/Purge Legacy Apple Apps.sh
+++ Library/Purge Legacy Apple Apps.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-  applications_list=(
-    "Pages.app"
-    "Numbers.app"
-    "Keynote.app"
-    "GarageBand.app"
-    "iMovie.app"
-  )
+applications_list=(
+  "Pages.app"
+  "Numbers.app"
+  "Keynote.app"
+  "GarageBand.app"
+  "iMovie.app"
+)
 
-  for applications in "${applications_list[@]}"; do
-    if [ -s "/Applications/${applications}" ]; then
-      if [ $(mdls -raw -name kMDItemAppStoreHasReceipt /Applications/${applications}) == "(null)" ]; then
-        echo "==> Remove legacy '/Applications/${applications}'"
-        rm -rf "/Applications/${applications}"
-      else
-        echo "==> Application '/Applications/${applications}' has already an App Store receipt"
-      fi
+for applications in "${applications_list[@]}"; do
+  if [ -s "/Applications/${applications}" ]; then
+    if [ "$(mdls -raw -name kMDItemAppStoreHasReceipt /Applications/"${applications}")" == "(null)" ]; then
+      echo "==> Remove legacy '/Applications/${applications}'"
+      rm -rf "/Applications/${applications}"
+    else
+      echo "==> Application '/Applications/${applications}' has already an App Store receipt"
     fi
-  done
+  fi
+done
 
-  /usr/local/bin/jamf "recon"
+/usr/local/bin/jamf "recon"
