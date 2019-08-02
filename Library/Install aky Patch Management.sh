83fa208788e16bbd1b7576ebd0db507f826b5389
ahousseini <contact@anver.ch>
2019-08-02 14:11:55 +0200

    Update Install aky Patch Management.sh

--- Library/Install aky Patch Management.sh
+++ Library/Install aky Patch Management.sh
@@ -1,54 +1,54 @@
 #!/bin/bash
 
-  # Remove existing aky binaries
+# Remove existing aky binaries
 
-  if [ -d "/usr/local/aky/" ]; then
-    rm -rf "/usr/local/aky/"
+if [ -d "/usr/local/aky/" ]; then
+  rm -rf "/usr/local/aky/"
+fi
+
+aky_binary=(
+  "aky"
+  "aria2c@el_capitan"
+  "aria2c@high_sierra"
+  "aria2c@mojave"
+  "aria2c@sierra"
+  "download-helper"
+  "install-helper"
+  "jq"
+  "mas-helper"
+  "notification-helper"
+  "rg"
+  "versioning-helper"
+)
+
+for ((i = 0; i < "${#aky_binary[@]}"; i++)); do
+
+  [ -L "/usr/local/bin/${aky_binary[$i]}" ] && unlink "/usr/local/bin/${aky_binary[$i]}"
+
+  if [ -s "/usr/local/bin/${aky_binary[$i]}" ]; then
+    rm -f "/usr/local/bin/${aky_binary[$i]}"
   fi
 
-  aky_binary=(
-    "aky"
-    "aria2c@el_capitan"
-    "aria2c@high_sierra"
-    "aria2c@mojave"
-    "aria2c@sierra"
-    "download-helper"
-    "install-helper"
-    "jq"
-    "mas-helper"
-    "notification-helper"
-    "rg"
-    "versioning-helper"
-  )
+done
 
-  for ((i = 0; i < "${#aky_binary[@]}"; i++)); do
+# Install aky binaries
 
-    [ -L "/usr/local/bin/${aky_binary[$i]}" ] && unlink "/usr/local/bin/${aky_binary[$i]}"
+if [ ! -d "/usr/local/bin" ]; then
+  mkdir -p "/usr/local/bin"
+fi
 
-    if [ -s "/usr/local/bin/${aky_binary[$i]}" ]; then
-      rm -f "/usr/local/bin/${aky_binary[$i]}"
-    fi
+aky_binary=(
+  "aky"
+  "rg"
+  "csc"
+  "jq"
+  "aria2c"
+  "tmpDir"
+)
 
-  done
-
-  # Install aky binaries
-
-  if [ ! -d "/usr/local/bin" ]; then
-    /bin/mkdir -p "/usr/local/bin"
-  fi
-
-  aky_binary=(
-    "aky"
-    "rg"
-    "csc"
-    "jq"
-    "aria2c"
-    "tmpDir"
-  )
-
-  for ((i = 0; i < "${#aky_binary[@]}"; i++)); do
-    /bin/rm -f "/usr/local/bin/${aky_binary[$i]}"
-    /usr/bin/curl -so "/usr/local/bin/${aky_binary[$i]}" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/aky-src/${aky_binary[$i]}"
-    /usr/sbin/chown root:wheel "/usr/local/bin/${aky_binary[$i]}"
-    /bin/chmod +x "/usr/local/bin/${aky_binary[$i]}"
-  done
+for ((i = 0; i < "${#aky_binary[@]}"; i++)); do
+  rm -f "/usr/local/bin/${aky_binary[$i]}"
+  curl -so "/usr/local/bin/${aky_binary[$i]}" "https://raw.githubusercontent.com/jedetaste/helper/master/bin/aky-src/${aky_binary[$i]}"
+  chown root:wheel "/usr/local/bin/${aky_binary[$i]}"
+  chmod +x "/usr/local/bin/${aky_binary[$i]}"
+done
