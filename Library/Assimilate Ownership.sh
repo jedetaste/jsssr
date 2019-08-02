539ce1e0a8605348b2de1260b656326ba425b1c4
ahousseini <contact@anver.ch>
2019-08-02 14:00:40 +0200

    Update Assimilate Ownership.sh

--- Library/Assimilate Ownership.sh
+++ Library/Assimilate Ownership.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-  applicationPath=(
-    '/Applications/Slack.app'
-    '/Applications/Scratch 2.app'
-    '/Applications/Dropbox.app'
-    '/Applications/Minecraft.app'
-    '/Applications/Lernprogramme/TastaBasta Training/'
-    '/Applications/Lernprogramme/Blitzrechnen/'
-    '/Applications/Lernprogramme/Clin dœil.app'
-    '/Applications/Lernprogramme/Mille feuilles.app'
-    '/Applications/Lernprogramme/New Headway'
-    '/Applications/ClinDoeil.app'
-    '/Applications/Millefeuilles.app'
-    '/Applications/Lernprogramme/New Inspiration IC/'
-  )
+applicationPath=(
+  '/Applications/Slack.app'
+  '/Applications/Scratch 2.app'
+  '/Applications/Dropbox.app'
+  '/Applications/Minecraft.app'
+  '/Applications/Lernprogramme/TastaBasta Training/'
+  '/Applications/Lernprogramme/Blitzrechnen/'
+  '/Applications/Lernprogramme/Clin dœil.app'
+  '/Applications/Lernprogramme/Mille feuilles.app'
+  '/Applications/Lernprogramme/New Headway'
+  '/Applications/ClinDoeil.app'
+  '/Applications/Millefeuilles.app'
+  '/Applications/Lernprogramme/New Inspiration IC/'
+)
 
-  for ((i = 0; i < "${#applicationPath[@]}"; i++)); do
-    if [ -s "${applicationPath[$i]}" ]; then
-      echo "Assimilate ownership for '${applicationPath[$i]}'"
-      /usr/local/bin/assimilateownership --path "${applicationPath[$i]}"
-    fi
-  done
+for ((i = 0; i < "${#applicationPath[@]}"; i++)); do
+  if [ -s "${applicationPath[$i]}" ]; then
+    echo "Assimilate ownership for '${applicationPath[$i]}'"
+    /usr/local/bin/assimilateownership --path "${applicationPath[$i]}"
+  fi
+done
