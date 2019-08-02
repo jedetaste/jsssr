1fb9035a5f3ff6bbb4d65f01d3d93ce3efc6cc12
ahousseini <contact@anver.ch>
2019-08-02 14:10:29 +0200

    Update Install VirtualBox.sh

--- Library/Install VirtualBox.sh
+++ Library/Install VirtualBox.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-  /usr/local/bin/aky virtualbox
+/usr/local/bin/aky virtualbox
 
-  SET_UID_BINARIES="VBoxNetAdpCtl"
-  SET_UID_BINARIES="${SET_UID_BINARIES} VBoxHeadless VirtualBoxVM VBoxNetDHCP VBoxNetNAT"
-  for bin in ${SET_UID_BINARIES}; do
-    echo "==> Adjust permissions for '/Applications/VirtualBox.app/Contents/MacOS/${bin}'"
-    chmod u+s "/Applications/VirtualBox.app/Contents/MacOS/${bin}"
-  done
+SET_UID_BINARIES="VBoxNetAdpCtl"
+SET_UID_BINARIES="${SET_UID_BINARIES} VBoxHeadless VirtualBoxVM VBoxNetDHCP VBoxNetNAT"
+for bin in ${SET_UID_BINARIES}; do
+  echo "==> Adjust permissions for '/Applications/VirtualBox.app/Contents/MacOS/${bin}'"
+  chmod u+s "/Applications/VirtualBox.app/Contents/MacOS/${bin}"
+done
 
-  version=$( curl --silent --insecure "https://download.virtualbox.org/virtualbox/LATEST.TXT" )
-  echo "==> Install 'VirtualBox ${version} Oracle VM VirtualBox Extension Pack'"
+version=$(curl --silent --insecure "https://download.virtualbox.org/virtualbox/LATEST.TXT")
+echo "==> Install 'VirtualBox ${version} Oracle VM VirtualBox Extension Pack'"
 
-  curl \
-    --silent \
-    --output "/private/tmp/Oracle_VM_VirtualBox_Extension_Pack-${version}.vbox-extpack" \
-    "https://download.virtualbox.org/virtualbox/${version}/Oracle_VM_VirtualBox_Extension_Pack-${version}.vbox-extpack"
+curl \
+  --silent \
+  --output "/private/tmp/Oracle_VM_VirtualBox_Extension_Pack-${version}.vbox-extpack" \
+  "https://download.virtualbox.org/virtualbox/${version}/Oracle_VM_VirtualBox_Extension_Pack-${version}.vbox-extpack"
 
-  echo "y" | /usr/local/bin/VBoxManage extpack install --replace "/private/tmp/Oracle_VM_VirtualBox_Extension_Pack-${version}.vbox-extpack"
+echo "y" | /usr/local/bin/VBoxManage extpack install --replace "/private/tmp/Oracle_VM_VirtualBox_Extension_Pack-${version}.vbox-extpack"
 
-  if [ -s "/private/tmp/Oracle_VM_VirtualBox_Extension_Pack-${version}.vbox-extpack" ]; then
-    rm -rf "/private/tmp/Oracle_VM_VirtualBox_Extension_Pack-${version}.vbox-extpack"
-  fi
+if [ -s "/private/tmp/Oracle_VM_VirtualBox_Extension_Pack-${version}.vbox-extpack" ]; then
+  rm -rf "/private/tmp/Oracle_VM_VirtualBox_Extension_Pack-${version}.vbox-extpack"
+fi
