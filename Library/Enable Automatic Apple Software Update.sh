747f3eedad72961f83b7f8bc5a964dab632bc69f
ahousseini <contact@anver.ch>
2019-08-20 23:08:25 +0200

    Update Enable Automatic Apple Software Update.sh

--- Library/Enable Automatic Apple Software Update.sh
+++ Library/Enable Automatic Apple Software Update.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-  major_os_version=$(sw_vers -productVersion | awk -F. '{print $2}')
+major_os_version=$(sw_vers -productVersion | awk -F. '{print $2}')
 
-  property_list="/Library/Preferences/com.apple.SoftwareUpdate.plist"
+property_list="/Library/Preferences/com.apple.SoftwareUpdate.plist"
 
-  defaults write "${property_list}" AutomaticCheckEnabled -bool true
-  defaults write "${property_list}" AutomaticDownload -bool true
-  defaults write "${property_list}" ConfigDataInstall -bool true
-  defaults write "${property_list}" CriticalUpdateInstall -bool true
+defaults write "${property_list}" AutomaticCheckEnabled -bool true
+defaults write "${property_list}" AutomaticDownload -bool true
+defaults write "${property_list}" ConfigDataInstall -bool true
+defaults write "${property_list}" CriticalUpdateInstall -bool true
 
-  if [[ "${major_os_version}" -ge 14 ]]; then
-    defaults write "${property_list}" AutomaticallyInstallMacOSUpdates -bool true
-    defaults write "${property_list}" AutoUpdate -bool true
-  fi
+if [[ "${major_os_version}" -ge 14 ]]; then
+  defaults write "${property_list}" AutomaticallyInstallMacOSUpdates -bool true
+  defaults write "${property_list}" AutoUpdate -bool true
+fi
 
-  property_list="/Library/Preferences/com.apple.commerce.plist"
+property_list="/Library/Preferences/com.apple.commerce.plist"
 
-  if [[ "${major_os_version}" -ge 10 ]] && [[ "${major_os_version}" -lt 14 ]]; then
-    defaults write "${property_list}" AutoUpdateRestartRequired -bool true
-  fi
+if [[ "${major_os_version}" -ge 10 ]] && [[ "${major_os_version}" -lt 14 ]]; then
+  defaults write "${property_list}" AutoUpdateRestartRequired -bool true
+fi
