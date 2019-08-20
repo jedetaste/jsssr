7dfe9d4d5c9df88c01b86820a5df33dbc7b30a58
ahousseini <contact@anver.ch>
2019-08-20 23:08:23 +0200

    Update Enable ARD.sh

--- Library/Enable ARD.sh
+++ Library/Enable ARD.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-  # Enable SSH and add admin to SSH group
+# Enable SSH and add admin to SSH group
 
-  /usr/sbin/systemsetup -setremotelogin on
-  /usr/sbin/dseditgroup -o edit -n /Local/Default -a admin -t user com.apple.access_ssh
-  
-  # Re-Configure ARD and activate for User admin and gucken 
-  
-  kickstart="/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart"
-  
-  if [ ! -z "$(id -u gucken)" ]; then
-    echo "User id gucken exists"
-    "${kickstart}" -activate -configure -allowAccessFor -specifiedUsers
-    "${kickstart}" -configure -users admin,gucken -access -on -privs -all
-    /usr/bin/defaults write /Library/Preferences/com.apple.loginwindow HiddenUsersList -array-add gucken
-  else
-    echo "User id gucken does not exist"
-    "${kickstart}" -activate -configure -allowAccessFor -specifiedUsers
-    "${kickstart}" -configure -users admin -access -on -privs -all
-  fi
-  
-  # Deactivate Screen Sharing Request Login
-  
-  /usr/bin/defaults write "/Library/Preferences/com.apple.RemoteManagement" ScreenSharingReqPermEnabled -bool NO
+/usr/sbin/systemsetup -setremotelogin on
+/usr/sbin/dseditgroup -o edit -n /Local/Default -a admin -t user com.apple.access_ssh
+
+# Re-Configure ARD and activate for User admin and gucken
+
+kickstart="/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart"
+
+if [ -n "$(id -u gucken)" ]; then
+  echo "User id gucken exists"
+  "${kickstart}" -activate -configure -allowAccessFor -specifiedUsers
+  "${kickstart}" -configure -users admin,gucken -access -on -privs -all
+  /usr/bin/defaults write /Library/Preferences/com.apple.loginwindow HiddenUsersList -array-add gucken
+else
+  echo "User id gucken does not exist"
+  "${kickstart}" -activate -configure -allowAccessFor -specifiedUsers
+  "${kickstart}" -configure -users admin -access -on -privs -all
+fi
+
+# Deactivate Screen Sharing Request Login
+
+/usr/bin/defaults write "/Library/Preferences/com.apple.RemoteManagement" ScreenSharingReqPermEnabled -bool NO
