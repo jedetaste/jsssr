b79916b83e70163e1771671f3a9757211d073dc5
ahousseini <contact@anver.ch>
2019-08-20 23:05:31 +0200

    Update Install SchoolCraftPremiumContent.sh

--- Library/Install SchoolCraftPremiumContent.sh
+++ Library/Install SchoolCraftPremiumContent.sh
@@ -1,47 +1,48 @@
 #!/bin/bash
 
-  version="2019.2.4.10"
-  download="https://www.hidrive.strato.com/wget/TpAqjsrr"
+version="2019.2.4.10"
+download="https://www.hidrive.strato.com/wget/TpAqjsrr"
 
+if [ -s "/Library/Application Support/WorksheetCrafter/SchoolCraft Premium Content.bundle" ]; then
+  version_installed=$(cat <"/Library/Application Support/WorksheetCrafter/SchoolCraft Premium Content.bundle/version.txt" | tr -d '[:space:]')
+  if [ "${version_installed}" = "${version}" ]; then
+    echo "==> School Craft Premium Content is up-to-date with v${version}" && exit 0
+  fi
+fi
+
+tmpDir=$(/usr/local/bin/tmpDir)
+
+echo "==> Download '${download}'"
+curl -s -o "${tmpDir}/SchoolCraftPremiumContent.pkg" "${download}"
+
+if [ -s "${tmpDir}/SchoolCraftPremiumContent.pkg" ]; then
+
+  echo "==> Prepare '${tmpDir}/SchoolCraftPremiumContent.pkg'"
+  pkgutil --expand-full "${tmpDir}/SchoolCraftPremiumContent.pkg" "${tmpDir}/SchoolCraftPremiumContent-expanded.pkg"
+
+  echo "==> Extract Payload 'SchoolCraft Premium Content.bundle'"
   if [ -s "/Library/Application Support/WorksheetCrafter/SchoolCraft Premium Content.bundle" ]; then
-    version_installed=$(cat "/Library/Application Support/WorksheetCrafter/SchoolCraft Premium Content.bundle/version.txt" | tr -d '[:space:]')
-    if [ "${version_installed}" = "${version}" ]; then
-      echo "==> School Craft Premium Content is up-to-date with v${version}" && exit 0
-    fi
+    rm -rf "/Library/Application Support/WorksheetCrafter/SchoolCraft Premium Content.bundle"
   fi
 
-  tmpDir=$(/usr/local/bin/tmpDir)
+  mkdir -p "/Library/Application Support/WorksheetCrafter"
 
-  echo "==> Download '${download}'"
-  curl -s -o "${tmpDir}/SchoolCraftPremiumContent.pkg" "${download}"
+  mv "${tmpDir}/SchoolCraftPremiumContent-expanded.pkg/SchoolCraftPremiumContent.pkg/Payload/tmp/SchoolCraft Premium Content.bundle" "/Library/Application Support/WorksheetCrafter/SchoolCraft Premium Content.bundle"
+  chmod -R 755 "/Library/Application Support/WorksheetCrafter/SchoolCraft Premium Content.bundle"
 
-  if [ -s "${tmpDir}/SchoolCraftPremiumContent.pkg" ]; then
-
-    echo "==> Prepare '${tmpDir}/SchoolCraftPremiumContent.pkg'"
-    pkgutil --expand-full "${tmpDir}/SchoolCraftPremiumContent.pkg" "${tmpDir}/SchoolCraftPremiumContent-expanded.pkg"
-
-    echo "==> Extract Payload 'SchoolCraft Premium Content.bundle'"
-    if [ -s "/Library/Application Support/WorksheetCrafter/SchoolCraft Premium Content.bundle" ]; then
-      rm -rf "/Library/Application Support/WorksheetCrafter/SchoolCraft Premium Content.bundle"
-    fi
-
-    mkdir -p "/Library/Application Support/WorksheetCrafter"
-
-    mv "${tmpDir}/SchoolCraftPremiumContent-expanded.pkg/SchoolCraftPremiumContent.pkg/Payload/tmp/SchoolCraft Premium Content.bundle" "/Library/Application Support/WorksheetCrafter/SchoolCraft Premium Content.bundle"
-    chmod -R 755 "/Library/Application Support/WorksheetCrafter/SchoolCraft Premium Content.bundle"
-    
-    for user in $(ls /Users | grep -v Shared | grep -v Guest | grep -v '.localized'); do
-      if [ -s "/Users/${user}/Pictures/SchoolCraft Premium Content.bundle" ]; then
-        rm -rf "/Users/${user}/Pictures/SchoolCraft Premium Content.bundle"
-        echo "==> Symlink '/Users/${user}/Pictures/SchoolCraft Premium Content.bundle'"
-        ln -s "/Library/Application Support/WorksheetCrafter/SchoolCraft Premium Content.bundle" "/Users/${user}/Pictures"
-      elif [ ! -s "/Users/${user}/Pictures/SchoolCraft Premium Content.bundle" ]; then
-        echo "==> Symlink '/Users/${user}/Pictures/SchoolCraft Premium Content.bundle'"
-        ln -s "/Library/Application Support/WorksheetCrafter/SchoolCraft Premium Content.bundle" "/Users/${user}/Pictures"
+  for user in /Users/*; do
+    if [ ! "$(basename "${user}")" = "Shared" ]; then
+      if [ -s "${user}/Pictures/SchoolCraft Premium Content.bundle" ]; then
+        rm -rf "${user}/Pictures/SchoolCraft Premium Content.bundle"
+        echo "==> Symlink '${user}/Pictures/SchoolCraft Premium Content.bundle'"
+        ln -s "/Library/Application Support/WorksheetCrafter/SchoolCraft Premium Content.bundle" "${user}/Pictures"
+      elif [ ! -s "${user}/Pictures/SchoolCraft Premium Content.bundle" ]; then
+        echo "==> Symlink '${user}/Pictures/SchoolCraft Premium Content.bundle'"
+        ln -s "/Library/Application Support/WorksheetCrafter/SchoolCraft Premium Content.bundle" "${user}/Pictures"
       fi
-    done
+    fi
+  done
 
-  fi
+fi
 
-  rm -rf "${tmpDir}"
-
+rm -rf "${tmpDir}"
