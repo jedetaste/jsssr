90ebfd64f3d07c960dba049a31c63cf36e7c2988
ahousseini <contact@anver.ch>
2019-08-23 14:45:48 +0200

    Update Firefox Bootstrap.sh

--- Library/Firefox Bootstrap.sh
+++ Library/Firefox Bootstrap.sh
@@ -1,52 +1,38 @@
 #!/bin/bash
 
-mkdir -p "/System/Library/User Template/Non_localized/Library/Application Support/Firefox/"
-profiles="/System/Library/User Template/Non_localized/Library/Application Support/Firefox/profiles.ini"
+for user_template in "/System/Library/User Template"/*; do
 
-function profileCreator() {
+  mkdir -p "${user_template}/Library/Application Support/Firefox/Profiles/any.default"
 
-  echo "[General]" >>"${profiles}"
-  echo "StartWithLastProfile=1" >>"${profiles}"
-  echo "[Profile0]" >>"${profiles}"
-  echo "Name=default" >>"${profiles}"
-  echo "IsRelative=1" >>"${profiles}"
-  echo "Path=Profiles/any.default" >>"${profiles}"
-  echo "Default=1" >>"${profiles}"
+  touch "${user_template}/Library/Application Support/Firefox/profiles.ini"
 
-}
+  cat >> "${user_template}/Library/Application Support/Firefox/profiles.ini" <<EOF
+[Profile0]
+Name=default
+IsRelative=1
+Path=Profiles/any.default
+Default=1
 
-if [ -e "${profiles}" ]; then
-  rm -rf "${profiles}"
-  touch "${profiles}"
-  profileCreator
-else
-  touch "${profiles}"
-  profileCreator
-fi
+[General]
+StartWithLastProfile=1
+Version=2
+EOF
 
-mkdir -p "/System/Library/User Template/Non_localized/Library/Application Support/Firefox/Profiles/any.default"
-prefs="/System/Library/User Template/Non_localized/Library/Application Support/Firefox/Profiles/any.default/prefs.js"
+  touch "${user_template}/Library/Application Support/Firefox/Profiles/any.default/prefs.js"
 
-function prefCreator() {
+  cat >> "${user_template}/Library/Application Support/Firefox/Profiles/any.default/prefs.js" <<EOF
+user_pref("app.update.auto", false);
+user_pref("app.update.enabled", false);
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("browser.shell.didSkipDefaultBrowserCheckOnFirstRun", true);
+user_pref("browser.startup.homepage", "https://portal.office.com/");
+user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
+user_pref("browser.bookmarks.restore_default_bookmarks", false);
+EOF
 
-  firefoxVersion=$(defaults read "/Applications/Firefox.app/Contents/Info.plist" CFBundleShortVersionString)
+done
 
-  echo 'user_pref("app.update.auto", false);' >>"${prefs}"
-  echo 'user_pref("app.update.enabled", false);' >>"${prefs}"
-  echo 'user_pref("browser.shell.checkDefaultBrowser", false);' >>"${prefs}"
-  echo 'user_pref("browser.shell.didSkipDefaultBrowserCheckOnFirstRun", true);' >>"${prefs}"
-  echo 'user_pref("browser.startup.homepage", "https://google.ch");' >>"${prefs}"
-  echo 'user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);' >>"${prefs}"
-  echo 'user_pref("browser.bookmarks.restore_default_bookmarks", false);' >>"${prefs}"
-  echo "user_pref(\"browser.startup.homepage_override.mstone\", \"${firefoxVersion}\");" >>"${prefs}"
 
-}
 
-if [ -e "${prefs}" ]; then
-  rm -rf "${prefs}"
-  touch "${prefs}"
-  prefCreator
-else
-  touch "${prefs}"
-  prefCreator
-fi
+
+
