633cc6b045991c62b0e628f33d25796b85e8a1d4
ahousseini <contact@anver.ch>
2019-11-26 17:59:15 +0100

    Update

--- Library/Install macOS High Sierra.sh
+++ Library/Install macOS High Sierra.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+IFS='.' read -r os_major os_minor os_revision < <(sw_vers -productVersion)
+
 current_user=$(scutil <<<"show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }')
 user_language=$(su -l "${current_user}" -c "/usr/libexec/PlistBuddy -c 'print AppleLanguages:0' ~/Library/Preferences/.GlobalPreferences.plist")
 
@@ -7,6 +9,11 @@ jamf_helper="/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS
 
 installer_name="High Sierra"
 installer_version="10.13"
+
+if [ "${os_minor}" -lt 12 ]; then
+  echo "Error: Please run upgrade manually, as macOS Upgrade from OS X 10.11 or less is not supported"
+  exit 1
+fi
 
 if [ "$(sw_vers -productVersion | awk -F. '{print $2}')" -lt 11 ]; then
   if [[ ${user_language} == en* ]]; then
@@ -37,11 +44,18 @@ if [ -s "/usr/local/bin/erase-install" ]; then
   echo "Download and Install 'First_Boot_Recon.pkg'"
   curl -s -o "/tmp/First_Boot_Recon.pkg" "https://cdn-clients.anykeyit.ch/Static/First_Boot_Recon.pkg"
 
-  /usr/local/bin/erase-install \
-    --reinstall \
-    --os=${installer_version} \
-    --extras=/tmp/First_Boot_Recon.pkg \
-    --catalogurl=https://swscan.apple.com/content/catalogs/others/index-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog
+  if [ "${os_minor}" -lt 13 ]; then
+    /usr/local/bin/erase-install \
+      --reinstall \
+      --os=${installer_version} \
+      --extras=/tmp/First_Boot_Recon.pkg \
+      --catalogurl=https://swscan.apple.com/content/catalogs/others/index-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog
+  else
+    /usr/local/bin/erase-install \
+      --reinstall \
+      --os=${installer_version} \
+      --extras=/tmp/First_Boot_Recon.pkg
+  fi
 
 else
   echo "Binary 'erase-install' not found." && exit 1
