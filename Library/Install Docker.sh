1599da6633dbcc69fbb3d7db91d9fe89e940b173
ahousseini <contact@anver.ch>
2019-12-22 16:53:40 +0100

    Update Install Docker.sh

--- Library/Install Docker.sh
+++ Library/Install Docker.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-	/usr/local/bin/aky docker
-#	/Applications/Docker.app/Contents/MacOS/Docker --quit-after-install --unattended
-#	nohup /Applications/Docker.app/Contents/MacOS/Docker --unattended &
-#    defaults write /Applications/Docker.app/Contents/Info.plist SUEnableAutomaticChecks "1"
-#    defaults write /Applications/Docker.app/Contents/Info.plist SUAllowsAutomaticUpdates "1"
-	/bin/cp "/Applications/Docker.app/Contents/Library/LaunchServices/com.docker.vmnetd" "/Library/PrivilegedHelperTools/"
-	/bin/cp "/Applications/Docker.app/Contents/Resources/com.docker.vmnetd.plist" "/Library/LaunchDaemons/"
-	/bin/chmod 544 "/Library/PrivilegedHelperTools/com.docker.vmnetd"
-	/bin/chmod 644 "/Library/LaunchDaemons/com.docker.vmnetd.plist"
-	/bin/launchctl load "/Library/LaunchDaemons/com.docker.vmnetd.plist"
-exit 0
+/usr/local/bin/aky "docker"
+
+cp "/Applications/Docker.app/Contents/Library/LaunchServices/com.docker.vmnetd" "/Library/PrivilegedHelperTools/"
+cp "/Applications/Docker.app/Contents/Resources/com.docker.vmnetd.plist" "/Library/LaunchDaemons/"
+
+chmod 544 "/Library/PrivilegedHelperTools/com.docker.vmnetd"
+chmod 644 "/Library/LaunchDaemons/com.docker.vmnetd.plist"
+
+launchctl load "/Library/LaunchDaemons/com.docker.vmnetd.plist"
