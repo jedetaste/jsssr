3f9cb5eadb155c05e4238462845e691a13e446e6
ahousseini <contact@anver.ch>
2019-12-23 13:42:44 +0100

    Create Install SmartBear SoapUI.sh

---
+++ Library/Install SmartBear SoapUI.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# shellcheck disable=SC2154
+
+version="5.5.0"
+
+install_soapui() {
+
+  echo "=> Download 'https://s3.amazonaws.com/downloads.eviware/soapuios/${version}/SoapUI-${version}.dmg'"
+
+  curl -s -L -o "/tmp/SoapUI-${version}.dmg" "https://s3.amazonaws.com/downloads.eviware/soapuios/${version}/SoapUI-${version}.dmg"
+
+  extract_dir=$(dmg-extractor "/tmp/SoapUI-${version}.dmg")
+
+  echo "=> Run installer '${extract_dir}/SoapUI ${version} Installer.app/Contents/MacOS/JavaApplicationStub'"
+
+  "${extract_dir}/SoapUI ${version} Installer.app/Contents/MacOS/JavaApplicationStub" \
+    -q \
+    -VcreateDesktopLinkAction"${Boolean}"=false \
+    -Vsys.component.132"${Boolean}"=true \
+    -Vsys.component.714"${Boolean}"=false \
+    -Vsys.component.1263"${Boolean}"=false \
+    -Vsys.component.2393"${Boolean}"=false \
+    -VshowFileAction"${Boolean}"=false \
+    -Vsys.installationDir=/Applications \
+    -VexecutionLauncherAction"${Boolean}"=false
+
+}
+
+if [ -z "$(defaults read "/Applications/SoapUI-${version}.app/Contents/Info.plist" CFBundleShortVersionString 2>/dev/null)" ]; then
+  echo "=> SoapUI not installed"
+  install_soapui
+elif [ ! "$(defaults read "/Applications/SoapUI-${version}.app/Contents/Info.plist" CFBundleShortVersionString 2>/dev/null)" = "${version}" ]; then
+  echo "=> SoapUI outdated"
+  install_soapui
+else
+  echo "=> SoapUI installed and up-to-date (v${version})"
+  exit 0
+fi
