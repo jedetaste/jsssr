7716793f06d9c1690dbb991e5ea15a0ef56e54c9
ahousseini <contact@anver.ch>
2020-01-03 11:33:06 +0100

    Update Change Hostname To Computer Name.sh

--- Library/Change Hostname To Computer Name.sh
+++ Library/Change Hostname To Computer Name.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-  jamf="/usr/local/jamf/bin/jamf"
-  computerName=$(/usr/sbin/scutil --get ComputerName)
-
-  if [ -s "${jamf}" ]; then
-    "${jamf}" setComputerName -target / -name "${computerName}"
-  fi
+if [ -s "/usr/local/jamf/bin/jamf" ]; then
+  "/usr/local/jamf/bin/jamf" setComputerName -target / -name "$(scutil --get computer_name)"
+fi
