d36cf437eb5b03f6711d75fa18ef689f004a717f
ahousseini <contact@anver.ch>
2020-01-03 11:53:20 +0100

    Update Install HIN Client.sh

--- Library/Install HIN Client.sh
+++ Library/Install HIN Client.sh
@@ -1,62 +1,28 @@
 #!/bin/bash
 
 version="1.5.6-80"
-version_purged=${version//./_}
 
-echo "=> Download 'https://download.hin.ch/download/distribution/install/${version}/HINClient_macos_${version_purged}.dmg'"
+install_hin_client() {
 
-curl -s -o "/tmp/HINClient_macos_${version_purged}.dmg" \
-  "https://download.hin.ch/download/distribution/install/${version}/HINClient_macos_${version_purged}.dmg"
+  download="https://download.hin.ch/download/distribution/install/${version}/HINClient_macos_${version//./_}.dmg"
 
-if [ -s "/tmp/HINClient_macos_${version_purged}.dmg" ]; then
+  echo "=> Download '${download}'"
+  curl -s -o "/tmp/HINClient_macos_${version//./_}.dmg" "${download}"
 
-  mount_point="/tmp/mount_point"
-  mkdir "${mount_point}"
+  extract_dir=$(dmg-extractor "/tmp/HINClient_macos_${version//./_}.dmg")
 
-  dmg="/tmp/HINClient_macos_${version_purged}.dmg"
+  echo "=> Running installer at '${extract_dir}/Install HIN Client.app/Contents/Resources/install.sh'"
+  bash "${extract_dir}/Install HIN Client.app/Contents/Resources/install.sh"
 
-  tmp_mount_point_file=$(mktemp /tmp/dmg.XXX) &&
-    hdiutil attach -plist -nobrowse -readonly -noidme -mountrandom "${mount_point}" "${dmg}" >"${tmp_mount_point_file}" &&
-    loc=":system-entities:"
+}
 
-  num=$(/usr/libexec/PlistBuddy -c "Print :system-entities:" "${tmp_mount_point_file}" | grep -c Dict)
-
-  for i in $(seq 0 $((num - 1))); do
-    loc=":system-entities:${i}:mount-point"
-    loc_dev_entry=":system-entities:${i}:dev-entry"
-    volume_name=$(/usr/libexec/PlistBuddy -c "Print ${loc}" "${tmp_mount_point_file}" 2>/dev/null)
-    volume_name_dev_entry=$(/usr/libexec/PlistBuddy -c "Print ${loc_dev_entry}" "${tmp_mount_point_file}" 2>/dev/null)
-    if [ -n "${volume_name}" ] && [ -d "${volume_name}" ]; then
-      break
-    fi
-  done
-
-  echo "=> Run install script at '${volume_name}/Install HIN Client.app/Contents/Resources/install.sh'"
-  bash "${volume_name}/Install HIN Client.app/Contents/Resources/install.sh"
-
-  if [ -n "${volume_name}" ]; then
-    diskutil eject "${volume_name}" >/dev/null 2>&1
-    until [ ! -d "${volume_name}" ]; do
-      diskutil unmount force "${volume_name}" >/dev/null 2>&1
-    done
-  fi
-
-  if [ -n "${volume_name_dev_entry}" ]; then
-    diskutil eject "${volume_name_dev_entry}" >/dev/null 2>&1
-    until [ ! -d "${volume_name_dev_entry}" ]; do
-      diskutil unmount force "${volume_name_dev_entry}" >/dev/null 2>&1
-    done
-  fi
-
-  if [ -d "${mount_point}" ]; then
-    rm -rf "${mount_point}"
-  fi
-
-  if [ -s "${tmp_mount_point_file}" ]; then
-    rm -rf "${tmp_mount_point_file}"
-  fi
-
-  echo "=> Remove installer at '${dmg}'"
-  rm -f "${dmg}"
-
+if [ -z "$(defaults read "/Applications/HIN Client.app/Contents/Info.plist" CFBundleVersion 2>/dev/null)" ]; then
+  echo "=> HIN Client not installed"
+  install_hin_client
+elif [ ! "$(defaults read "/Applications/HIN Client.app/Contents/Info.plist" CFBundleVersion 2>/dev/null)" = "${version}" ]; then
+  echo "=> HIN Client outdated"
+  install_hin_client
+else
+  echo "=> HIN Client installed and up-to-date with v${version}"
+  exit 0
 fi
