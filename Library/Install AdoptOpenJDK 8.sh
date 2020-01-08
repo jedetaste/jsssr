cccabbca48ac18044ada6ec415d2967617048885
ahousseini <contact@anver.ch>
2020-01-08 13:36:36 +0100

    Update Install AdoptOpenJDK 8.sh

--- Library/Install AdoptOpenJDK 8.sh
+++ Library/Install AdoptOpenJDK 8.sh
@@ -6,7 +6,7 @@ build="09"
 sha256="6a21734cf0aa4e7f5ad74c2d232c9af8426a2e66a9909821b4c4d81be2aee035"
 download="https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u${version}-b${build}/OpenJDK8U-jdk_x64_mac_hotspot_8u${version}b${build}.pkg"
 
-tmpDir=$(/usr/local/bin/tmpDir)
+tmp_dir=$(/usr/local/bin/tmpDir)
 redirect_url=$(curl -w "%{redirect_url}" -o /dev/null -s "${download}")
 current_version=$(defaults read /Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Info.plist CFBundleGetInfoString 2>/dev/null)
 available_version="AdoptOpenJDK ${major_version}_${version}-b${build}"
@@ -15,13 +15,13 @@ install_jdk() {
 
   echo "=> Downloading '${download}'"
   echo "=> Downloading from '${redirect_url}'"
-  curl -s -o "${tmpDir}/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg" "${redirect_url}"
+  curl -s -o "${tmp_dir}/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg" "${redirect_url}"
 
-  if [ -s "${tmpDir}/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg" ]; then
-    if [ "$(shasum -a 256 -p "${tmpDir}/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg" | cut -d' ' -f1)" = "${sha256}" ]; then
+  if [ -s "${tmp_dir}/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg" ]; then
+    if [ "$(shasum -a 256 -p "${tmp_dir}/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg" | cut -d' ' -f1)" = "${sha256}" ]; then
       echo "=> Expected SHA-256 checksum is correct"
-      echo "=> Install '${tmpDir}/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg'"
-      installer -pkg "${tmpDir}/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg" -target /
+      echo "=> Install '${tmp_dir}/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg'"
+      installer -pkg "${tmp_dir}/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg" -target /
     else
       echo "SHA-256 checksum mismatch" && exit 1
     fi
@@ -44,4 +44,4 @@ else
   install_jdk "$@"
 fi
 
-rm -rf "${tmpDir}"
+rm -rf "${tmp_dir}"
