9bda3bb1bc34f98fe7b8a0b3a685b87c6f1236f2
ahousseini <contact@anver.ch>
2020-01-30 14:31:11 +0100

    Create Remove Oracle Java 8.sh

---
+++ Library/Remove Oracle Java 8.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+[ -s "/Library/Internet Plug-Ins/JavaAppletPlugin.plugin" ] && rm -rf "/Library/Internet Plug-Ins/JavaAppletPlugin.plugin"
+[ -s "/Library/PreferencePanes/JavaControlPanel.prefPane" ] && rm -rf "/Library/PreferencePanes/JavaControlPanel.prefPane"
