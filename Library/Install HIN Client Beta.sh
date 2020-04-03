4873e374da839fd1da4c6b74b90cb375a0e405ba
ahousseini <contact@anver.ch>
2020-04-03 14:36:54 +0200

    Update Install HIN Client Beta.sh

--- Library/Install HIN Client Beta.sh
+++ Library/Install HIN Client Beta.sh
@@ -1,13 +1,16 @@
-#!/bin/bash
+#!/bin/bash -x
+
+set_uid() {
+  proxysettings_wrapper_path="/Applications/HIN Client.app/Contents/Resources/app/proxysettings"
+  proxysettings_script_path="/Applications/HIN Client.app/Contents/Resources/app/proxysettings.sh"
+  chown 0:0 "${proxysettings_wrapper_path}"
+  chown 0:0 "${proxysettings_script_path}"
+  chmod 6755 "${proxysettings_wrapper_path}"
+}
 
 if [ "${1}" = "update" ]; then
   /usr/local/bin/aky --update "hinclient"
+  [ -d "/Applications/HIN Client.app" ] && set_uid
 else
-  /usr/local/bin/aky "hinclient"
+  /usr/local/bin/aky "hinclient" && set_uid
 fi
-
-proxysettings_wrapper_path="/Applications/HIN Client.app/Contents/Resources/app/proxysettings"
-proxysettings_script_path="/Applications/HIN Client.app/Contents/Resources/app/proxysettings.sh"
-chown 0:0 "${proxysettings_wrapper_path}"
-chown 0:0 "${proxysettings_script_path}"
-chmod 6755 "${proxysettings_wrapper_path}"
