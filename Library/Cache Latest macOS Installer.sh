8ff915308a2004552c42a4325122aa7a4b53f067
ahousseini <contact@anver.ch>
2020-05-03 22:54:59 +0200

    Update Cache Latest macOS Installer.sh

--- Library/Cache Latest macOS Installer.sh
+++ Library/Cache Latest macOS Installer.sh
@@ -5,7 +5,7 @@ IFS='.' read -r major minor revision < <(sw_vers -productVersion)
 if [ "${minor}" -gt 15 ]; then
   echo "Error: This Mac seems to run a beta of macOS" && exit 0
 elif [ ! "${minor}" -eq 15 ]; then
-  /usr/local/bin/erase-install --move --os=10.15
+  /usr/local/bin/erase-install --overwrite --os=10.15
 else
   echo "Computer is already on macOS 10.15"
 fi
