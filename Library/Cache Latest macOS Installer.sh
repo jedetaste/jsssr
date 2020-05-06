9206d924eb2836b0f900b21e51ba9efcacfe3d29
ahousseini <contact@anver.ch>
2020-05-06 18:09:15 +0200

    Update Cache Latest macOS Installer.sh

--- Library/Cache Latest macOS Installer.sh
+++ Library/Cache Latest macOS Installer.sh
@@ -8,12 +8,12 @@ if [ "${minor}" -gt 15 ]; then
 elif [ "${minor}" -lt 13 ]; then
   echo "=> Computer is running macOS ${major}.${minor}.${revision}"
   caffeinate -d -i -m -s &
-  /usr/local/bin/erase-install --catalogurl=https://swscan.apple.com/content/catalogs/others/index-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog --overwrite --os=10.15
+  /usr/local/bin/erase-install --catalogurl=https://swscan.apple.com/content/catalogs/others/index-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog --update --os=10.15
   killall caffeinate
 elif [ ! "${minor}" -eq 15 ]; then
   echo "=> Computer is running macOS ${major}.${minor}.${revision}"
   caffeinate -d -i -m -s &
-  /usr/local/bin/erase-install --overwrite --os=10.15
+  /usr/local/bin/erase-install --update --os=10.15
   killall caffeinate
 else
   echo "=> Computer is already on macOS 10.15"
