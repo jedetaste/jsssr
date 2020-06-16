2237fa8d956e1322447263bb42d76563f7bdff6d
Anver Housseini <anver@anykey.ch>
2020-06-16 13:52:43 +0200

    update

---
+++ Library/Enable Trackpad Tap To Click.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+su -l "$(/usr/local/bin/currentuser)" -c "defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true"
+
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
