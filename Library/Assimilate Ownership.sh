16e50b3bc01692b522776b3fa92daf61134f5c81
Anver Housseini <anver@anykey.ch>
2020-08-11 22:25:45 +0200

    update

--- Library/Assimilate Ownership.sh
+++ Library/Assimilate Ownership.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 application_path=(
-  '/Applications/Slack.app'
-  '/Applications/Spotify.app'
-  '/Applications/WhatsApp.app'
-  '/Applications/Scratch 2.app'
-  '/Applications/Dropbox.app'
-  '/Applications/Citrix Workspace.app'
-  '/Applications/Signal.app'
-  '/Applications/Minecraft.app'
-  '/Applications/Lernprogramme/TastaBasta Training/'
-  '/Applications/Lernprogramme/Blitzrechnen/'
-  '/Applications/Lernprogramme/Clin dœil.app'
-  '/Applications/Lernprogramme/Mille feuilles.app'
-  '/Applications/Lernprogramme/New Headway'
-  '/Applications/Lernprogramme/New Inspiration IC/'
-  '/Applications/ClinDoeil.app'
-  '/Applications/Millefeuilles.app'
+  "/Applications/Slack.app"
+  "/Applications/Spotify.app"
+  "/Applications/WhatsApp.app"
+  "/Applications/Scratch 2.app"
+  "/Applications/Dropbox.app"
+  "/Applications/Citrix Workspace.app"
+  "/Applications/Signal.app"
+  "/Applications/Minecraft.app"
+  "/Applications/Notion.app"
+  "/Applications/Lernprogramme/TastaBasta Training/"
+  "/Applications/Lernprogramme/Blitzrechnen/"
+  "/Applications/Lernprogramme/Clin dœil.app"
+  "/Applications/Lernprogramme/Mille feuilles.app"
+  "/Applications/Lernprogramme/New Headway"
+  "/Applications/Lernprogramme/New Inspiration IC/"
+  "/Applications/ClinDoeil.app"
+  "/Applications/Millefeuilles.app"
 )
 
 for application in "${application_path[@]}"; do
