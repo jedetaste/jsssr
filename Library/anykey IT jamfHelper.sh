3e695b2f844c9ff52d97d8483894b17e8736e4db
Anver Housseini <anver@anykey.ch>
2020-09-18 15:15:01 +0200

    Update anykey IT jamfHelper.sh

--- Library/anykey IT jamfHelper.sh
+++ Library/anykey IT jamfHelper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl --silent --output "/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/Resources/Message.png" "https://cdn-clients.anykeyit.ch/Static/Message.png"
+curl --silent --output "/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/Resources/Message.png" "https://cdn-cloud.anykeyit.ch/Static/Amy/Amy-weiss.png"
