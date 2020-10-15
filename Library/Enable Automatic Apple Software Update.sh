de112c6f3335aa41f21b9fc6dd92df6267d40cf7
Anver Housseini <anver@anykey.ch>
2020-10-15 10:12:27 +0200

    Update Enable Automatic Apple Software Update.sh

--- Library/Enable Automatic Apple Software Update.sh
+++ Library/Enable Automatic Apple Software Update.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+# shellcheck disable=SC2034
 
-major_os_version=$(sw_vers -productVersion | awk -F. '{print $2}')
+set -x
+
+OLDIFS="${IFS}"
+
+IFS='.' read -r osvers_major osvers_minor osvers_dot_version <<<"$(/usr/bin/sw_vers -productVersion)"
+
+IFS="${OLDIFS}"
 
 property_list="/Library/Preferences/com.apple.SoftwareUpdate.plist"
 
@@ -8,15 +15,14 @@ defaults write "${property_list}" AutomaticCheckEnabled -bool true
 defaults write "${property_list}" AutomaticDownload -bool true
 defaults write "${property_list}" ConfigDataInstall -bool true
 defaults write "${property_list}" CriticalUpdateInstall -bool true
-defaults write "${property_list}" AllowPreReleaseInstallation -bool false
+defaults write "${property_list}" AllowPreReleaseInstallation -bool true
 
-if [[ "${major_os_version}" -ge 14 ]]; then
+if [[ (${osvers_major} -eq 10 && ${osvers_minor} -ge 14) || (${osvers_major} -eq 11) ]]; then
   defaults write "${property_list}" AutomaticallyInstallMacOSUpdates -bool true
-  defaults write "${property_list}" AutoUpdate -bool true
 fi
 
 property_list="/Library/Preferences/com.apple.commerce.plist"
 
-if [[ "${major_os_version}" -ge 10 ]] && [[ "${major_os_version}" -lt 14 ]]; then
+if [[ (${osvers_major} -eq 10 && (${osvers_minor} -ge 10 && ${osvers_minor} -lt 14)) ]]; then
   defaults write "${property_list}" AutoUpdateRestartRequired -bool true
 fi
