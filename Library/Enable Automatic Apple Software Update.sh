a1c155dc207a4282a897062889767773047ce879
Anver Housseini <anver@anykey.ch>
2020-10-15 10:26:38 +0200

    Update Enable Automatic Apple Software Update.sh

--- Library/Enable Automatic Apple Software Update.sh
+++ Library/Enable Automatic Apple Software Update.sh
@@ -23,7 +23,8 @@ fi
 
 property_list="/Library/Preferences/com.apple.commerce.plist"
 
+defaults write "${property_list}" AutoUpdate -bool true
+
 if [[ (${osvers_major} -eq 10 && (${osvers_minor} -ge 10 && ${osvers_minor} -lt 14)) ]]; then
   defaults write "${property_list}" AutoUpdateRestartRequired -bool true
-  defaults write "${property_list}" AutoUpdate -bool true
 fi
