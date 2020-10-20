3771f5a8bc0cc7a04e392b1572746d853a95d714
Anver Housseini <anver@anykey.ch>
2020-10-20 16:03:01 +0200

    Update Install Adobe Creative Cloud.sh

--- Library/Install Adobe Creative Cloud.sh
+++ Library/Install Adobe Creative Cloud.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-/usr/local/bin/aky adobecreativecloudinstaller
+url="https://ccmdl.adobe.com/AdobeProducts/KCCC/CCD/5_2/osx10/ACCCx5_2_0_436.dmg"
 
-sudo /tmp/Install.app/Contents/MacOS/Install --mode=silent
+echo "=> Download '${url}'"
+
+curl -s -o "/tmp/adobe-creative-cloud.dmg" "${url}"
+
+echo "=> Extract '/tmp/adobe-creative-cloud.dmg'"
+
+extract_dir=$(dmg-extractor "/tmp/adobe-creative-cloud.dmg")
+
+echo "=> "
+
+sudo "${extract_dir}/Install.app/Contents/MacOS/Install" --mode=silent
+
+rm -f "/tmp/adobe-creative-cloud.dmg"
+rm -rf "$(dirname "${extract_dir}")"
