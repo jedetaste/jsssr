ea497eab58954059a18c9660350d56f16aa1dc88
Anver Housseini <anver@anykey.ch>
2020-10-29 20:24:51 +0100

    Update Set PDF UTI Default Application.sh

--- Library/Set PDF UTI Default Application.sh
+++ Library/Set PDF UTI Default Application.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-  su -l $(/usr/local/bin/currentuser) -c "/usr/local/bin/duti -s com.adobe.Reader com.adobe.pdf all"
-  su -l $(/usr/local/bin/currentuser) -c "/usr/local/bin/duti -s com.adobe.Reader public.composite-content all"
+su -l $(/usr/local/bin/currentuser) -c "/usr/local/bin/duti -s com.adobe.Reader com.adobe.pdf all"
+su -l $(/usr/local/bin/currentuser) -c "/usr/local/bin/duti -s com.adobe.Reader public.composite-content all"
