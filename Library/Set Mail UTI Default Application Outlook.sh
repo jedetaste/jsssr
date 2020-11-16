b4f92974d00263f7b18ed6cab954eba14c3e75a4
Anver Housseini <anver@anykey.ch>
2020-11-16 22:47:44 +0100

    Update Set Mail UTI Default Application Outlook.sh

--- Library/Set Mail UTI Default Application Outlook.sh
+++ Library/Set Mail UTI Default Application Outlook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-  su -l $(/usr/local/bin/currentuser) -c "/usr/local/bin/duti -s com.microsoft.outlook mailto"
-  su -l $(/usr/local/bin/currentuser) -c "/usr/local/bin/duti -s com.microsoft.outlook ics all"
-  su -l $(/usr/local/bin/currentuser) -c "/usr/local/bin/duti -s com.microsoft.outlook vcf all"
+su -l "$(/usr/local/bin/currentuser)" -c "/usr/local/bin/duti -s com.microsoft.outlook mailto"
+su -l "$(/usr/local/bin/currentuser)" -c "/usr/local/bin/duti -s com.microsoft.outlook ics all"
+su -l "$(/usr/local/bin/currentuser)" -c "/usr/local/bin/duti -s com.microsoft.outlook vcf all"
