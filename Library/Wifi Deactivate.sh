74905f3160f361c84bb41c0c9aedc17c3c3ce4c5
Anver Housseini <anver@anykey.ch>
2020-11-16 22:46:54 +0100

    Update Wifi Deactivate.sh

--- Library/Wifi Deactivate.sh
+++ Library/Wifi Deactivate.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-  ACTIVEIF=$(/usr/sbin/netstat -rn 2>&1 | /usr/bin/grep -m 1 'default' | /usr/bin/awk '{ print $6 }')
-  WIFIIF=$(/usr/sbin/networksetup -listallhardwareports | grep -A 1 Wi-Fi | awk '/Device/{ print $2 }')
-  WIFIPWR=$(/usr/sbin/networksetup -getairportpower $WIFIIF | sed 's/Wi-Fi Power.*: //g')
+ACTIVEIF=$(netstat -rn 2>&1 | /usr/bin/grep -m 1 'default' | /usr/bin/awk '{ print $6 }')
+WIFIIF=$(networksetup -listallhardwareports | grep -A 1 Wi-Fi | awk '/Device/{ print $2 }')
+WIFIPWR=$(networksetup -getairportpower "${WIFIIF}" | sed 's/Wi-Fi Power.*: //g')
 
-  if [ "$ACTIVEIF" == "$WIFIIF" ]; then
+if [ "${ACTIVEIF}" == "${WIFIIF}" ]; then
+  exit 0
+else
+  if [ ! "${WIFIPWR}" = "On" ]; then
     exit 0
   else
-    if [ "$WIFIPWR" != "On" ]; then
-      exit 0
-    else
-      /usr/sbin/networksetup -setairportpower $WIFIIF Off
-    fi
+    networksetup -setairportpower "${WIFIIF}" Off
   fi
+fi
