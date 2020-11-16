741d2be8c40fe000db974d85343b1916e8a9bd07
Anver Housseini <anver@anykey.ch>
2020-11-16 23:05:14 +0100

    Update Enable ARD.sh

--- Library/Enable ARD.sh
+++ Library/Enable ARD.sh
@@ -2,8 +2,8 @@
 
 # Enable SSH and add admin to SSH group
 
-/usr/sbin/systemsetup -setremotelogin on
-/usr/sbin/dseditgroup -o edit -n /Local/Default -a admin -t user com.apple.access_ssh
+systemsetup -setremotelogin on
+dseditgroup -o edit -n /Local/Default -a admin -t user com.apple.access_ssh
 
 # Re-Configure ARD and activate for User admin and gucken
 
@@ -13,7 +13,7 @@ if [ -n "$(id -u gucken)" ]; then
   echo "User id gucken exists"
   "${kickstart}" -activate -configure -allowAccessFor -specifiedUsers
   "${kickstart}" -configure -users admin,gucken -access -on -privs -all
-  /usr/bin/defaults write /Library/Preferences/com.apple.loginwindow HiddenUsersList -array-add gucken
+  defaults write /Library/Preferences/com.apple.loginwindow HiddenUsersList -array-add gucken
 else
   echo "User id gucken does not exist"
   "${kickstart}" -activate -configure -allowAccessFor -specifiedUsers
@@ -22,4 +22,4 @@ fi
 
 # Deactivate Screen Sharing Request Login
 
-/usr/bin/defaults write "/Library/Preferences/com.apple.RemoteManagement" ScreenSharingReqPermEnabled -bool NO
+defaults write "/Library/Preferences/com.apple.RemoteManagement" ScreenSharingReqPermEnabled -bool NO
