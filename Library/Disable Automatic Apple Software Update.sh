15de04ad7b557e78fda3a035b28770b42092b8cb
Anver Housseini <anver@anykey.ch>
2020-11-16 22:30:45 +0100

    Update Disable Automatic Apple Software Update.sh

--- Library/Disable Automatic Apple Software Update.sh
+++ Library/Disable Automatic Apple Software Update.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-# shellcheck disable=SC2034
-
-set -x
-
-OLDIFS="${IFS}"
-
-IFS='.' read -r osvers_major osvers_minor osvers_dot_version <<<"$(/usr/bin/sw_vers -productVersion)"
-
-IFS="${OLDIFS}"
+# shellcheck disable=SC2071
 
 property_list="/Library/Preferences/com.apple.SoftwareUpdate.plist"
 
@@ -17,7 +9,7 @@ defaults write "${property_list}" ConfigDataInstall -bool false
 defaults write "${property_list}" CriticalUpdateInstall -bool false
 defaults write "${property_list}" AllowPreReleaseInstallation -bool false
 
-if [[ (${osvers_major} -eq 10 && ${osvers_minor} -ge 14) || (${osvers_major} -eq 11) ]]; then
+if [[ "$(sw_vers -buildVersion)" > "17" ]]; then
   defaults write "${property_list}" AutomaticallyInstallMacOSUpdates -bool false
 fi
 
@@ -25,6 +17,6 @@ property_list="/Library/Preferences/com.apple.commerce.plist"
 
 defaults write "${property_list}" AutoUpdate -bool false
 
-if [[ (${osvers_major} -eq 10 && (${osvers_minor} -ge 10 && ${osvers_minor} -lt 14)) ]]; then
+if [[ "$(sw_vers -buildVersion)" > "13" ]] && [[ "$(sw_vers -buildVersion)" < "17" ]]; then
   defaults write "${property_list}" AutoUpdateRestartRequired -bool false
 fi
