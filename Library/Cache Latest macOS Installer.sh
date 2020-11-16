92ac9516bda938030fe9678cfb44be326541f73e
Anver Housseini <anver@anykey.ch>
2020-11-16 22:24:32 +0100

    Update Cache Latest macOS Installer.sh

--- Library/Cache Latest macOS Installer.sh
+++ Library/Cache Latest macOS Installer.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
+# shellcheck disable=SC2071
 
-IFS='.' read -r major minor revision < <(sw_vers -productVersion)
-
-if [ "${minor}" -gt 15 ]; then
-  echo "=> Computer is running macOS ${major}.${minor}.${revision}"
+if [[ "$(sw_vers -buildVersion)" > "20" ]]; then
+  echo "=> Mac is running Darwin $(sw_vers -buildVersion)"
   echo "Error: This Mac seems to run a beta of macOS" && exit 0
-elif [ "${minor}" -lt 13 ]; then
-  echo "=> Computer is running macOS ${major}.${minor}.${revision}"
+elif [[ "$(sw_vers -buildVersion)" < "17" ]]; then
+  echo "=> Mac is running Darwin $(sw_vers -buildVersion)"
   /usr/local/bin/erase-install --catalogurl=https://swscan.apple.com/content/catalogs/others/index-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog --update --os=10.15
 else
-  echo "=> Computer is running macOS ${major}.${minor}.${revision}"
-  /usr/local/bin/erase-install --update --os=10.15
+  echo "=> Mac is running Darwin $(sw_vers -buildVersion)"
+  /usr/local/bin/erase-install --update --os=11.0
 fi
