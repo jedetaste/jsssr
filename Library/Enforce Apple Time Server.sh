18b2aae3003a00883af858188c3ec84179c9f292
Anver Housseini <anver@anykey.ch>
2020-11-16 22:19:17 +0100

    Update Enforce Apple Time Server.sh

--- Library/Enforce Apple Time Server.sh
+++ Library/Enforce Apple Time Server.sh
@@ -2,25 +2,14 @@
 
 # Determine OS version and build version
 
-echo "==> Determine macOS Version"
-
-macos_vers() {
-  IFS='.' read -r major minor revision < <(/usr/bin/sw_vers -productVersion)
-}
-
-macos_vers
-
-if [ "${minor}" -gt 11 ]; then
-  echo "==> Mac is running macOS ${major}.${minor}.${revision} ($(sw_vers -buildVersion))"
-else
-  echo "==> Mac is running OS X ${major}.${minor}.${revision} ($(sw_vers -buildVersion))"
-fi
+echo "=> Determine macOS Version"
+echo "=> Mac is running Darwin $(sw_vers -buildVersion)"
 
 # Configure time settings
 
 echo "==> Set time server to 'time.euro.apple.com'"
 
-if [ "${minor}" -gt 13 ]; then
+if [[ "$(sw_vers -buildVersion)" > "17" ]]; then
   systemsetup -setusingnetworktime on
   sntp -sS "time.euro.apple.com"
 else
