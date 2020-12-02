c82cbe13e38bce2d613701d89751ced71fdeb938
Anver Housseini <anver@anykey.ch>
2020-12-02 12:08:07 +0100

    Update Install Command Line Tools.sh

--- Library/Install Command Line Tools.sh
+++ Library/Install Command Line Tools.sh
@@ -1,85 +1,34 @@
 #!/bin/bash
 
-# Installing the Xcode command line tools on 10.7.x or higher
+minimum_darwin_version="16"
 
-# Save current IFS state
+if [[ "$(sw_vers -buildVersion)" > "${minimum_darwin_version}" ]]; then
 
-OLDIFS=$IFS
+  if ! [[ -e "/Library/Developer/CommandLineTools/usr/bin/git" ]]; then
 
-IFS='.' read osvers_major osvers_minor osvers_dot_version <<<"$(/usr/bin/sw_vers -productVersion)"
+    chomp() {
+      printf "%s" "${1/"$'\n'"/}"
+    }
 
-# restore IFS to previous state
+    clt_placeholder="/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
 
-IFS=$OLDIFS
+    touch "${clt_placeholder}"
 
-cmd_line_tools_temp_file="/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
+    clt_label_command="softwareupdate -l | grep -B 1 -E 'Command Line Tools' | awk -F'*' '/^ *\\*/ {print \$2}' | sed -e 's/^ *Label: //' -e 's/^ *//' | sort -V | tail -n1"
 
-# Installing the latest Xcode command line tools on 10.9.x or higher
+    clt_label="$(chomp "$(/bin/bash -c "$clt_label_command")")"
 
-if [[ (${osvers_major} -eq 10 && ${osvers_minor} -ge 9) || (${osvers_major} -eq 11 && ${osvers_minor} -ge 0) ]]; then
+    if [[ -n "${clt_label}" ]]; then
+      echo "=> Installing ${clt_label}"
+      softwareupdate -i "${clt_label}"
+      rm -f "${clt_placeholder}"
+      xcode-select --switch "/Library/Developer/CommandLineTools"
+    fi
 
-  # Create the placeholder file which is checked by the softwareupdate tool
-  # before allowing the installation of the Xcode command line tools.
-
-  touch "$cmd_line_tools_temp_file"
-
-  # Identify the correct update in the Software Update feed with "Command Line Tools" in the name for the OS version in question.
-
-  if [[ (${osvers_major} -eq 10 && ${osvers_minor} -ge 15) || (${osvers_major} -eq 11 && ${osvers_minor} -ge 0) ]]; then
-    cmd_line_tools=$(softwareupdate -l | awk '/\*\ Label: Command Line Tools/ { $1=$1;print }' | sed 's/^[[ \t]]*//;s/[[ \t]]*$//;s/*//' | cut -c 9-)
-  elif [[ (${osvers_major} -eq 10 && ${osvers_minor} -gt 9) ]] && [[ (${osvers_major} -eq 10 && ${osvers_minor} -lt 15) ]]; then
-    cmd_line_tools=$(softwareupdate -l | awk '/\*\ Command Line Tools/ { $1=$1;print }' | grep "$macos_vers" | sed 's/^[[ \t]]*//;s/[[ \t]]*$//;s/*//' | cut -c 2-)
-  elif [[ (${osvers_major} -eq 10 && ${osvers_minor} -eq 9) ]]; then
-    cmd_line_tools=$(softwareupdate -l | awk '/\*\ Command Line Tools/ { $1=$1;print }' | grep "Mavericks" | sed 's/^[[ \t]]*//;s/[[ \t]]*$//;s/*//' | cut -c 2-)
+  else
+    echo "=> Command Line Tools already installed"
   fi
 
-  # Check to see if the softwareupdate tool has returned more than one Xcode
-  # command line tool installation option. If it has, use the last one listed
-  # as that should be the latest Xcode command line tool installer.
-
-  if (($(grep -c . <<<"$cmd_line_tools") > 1)); then
-    cmd_line_tools_output="$cmd_line_tools"
-    cmd_line_tools=$(printf "$cmd_line_tools_output" | tail -1)
-  fi
-
-  #Install the command line tools
-
-  softwareupdate -i "$cmd_line_tools" --verbose
-
-  # Remove the temp file
-
-  if [[ -f "$cmd_line_tools_temp_file" ]]; then
-    rm "$cmd_line_tools_temp_file"
-  fi
+else
+  echo "=> Command Line Tools require Darwin version ${minimum_darwin_version} or greater"
 fi
-
-# Installing the latest Xcode command line tools on 10.7.x and 10.8.x
-
-# on 10.7/10.8, instead of using the software update feed, the command line tools are downloaded
-# instead from public download URLs, which can be found in the dvtdownloadableindex:
-# https://devimages.apple.com.edgekey.net/downloads/xcode/simulators/index-3905972D-B609-49CE-8D06-51ADC78E07BC.dvtdownloadableindex
-
-if [[ (${osvers_major} -eq 10 && ${osvers_minor} -eq 7) || (${osvers_major} -eq 10 && ${osvers_minor} -eq 8) ]]; then
-
-  if [[ (${osvers_major} -eq 10 && ${osvers_minor} -eq 7) ]]; then
-    DMGURL=http://devimages.apple.com/downloads/xcode/command_line_tools_for_xcode_os_x_lion_april_2013.dmg
-  fi
-
-  if [[ (${osvers_major} -eq 10 && ${osvers_minor} -eq 8) ]]; then
-    DMGURL=http://devimages.apple.com/downloads/xcode/command_line_tools_for_osx_mountain_lion_april_2014.dmg
-  fi
-
-  TOOLS=cltools.dmg
-  curl "$DMGURL" -o "$TOOLS"
-  TMPMOUNT=$(/usr/bin/mktemp -d /tmp/clitools.XXXX)
-  hdiutil attach "$TOOLS" -mountpoint "$TMPMOUNT" -nobrowse
-  # The "-allowUntrusted" flag has been added to the installer
-  # command to accomodate for now-expired certificates used
-  # to sign the downloaded command line tools.
-  installer -allowUntrusted -pkg "$(find $TMPMOUNT -name '*.mpkg')" -target /
-  hdiutil detach "$TMPMOUNT"
-  rm -rf "$TMPMOUNT"
-  rm "$TOOLS"
-fi
-
-exit 0
