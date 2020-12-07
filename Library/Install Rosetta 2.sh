29c9d8dbb3ccff45a52d86108f6de6bfbc6d14fa
Claudio Gardini <c.gardini@anykeyit.ch>
2020-12-07 10:46:32 +0100

    update

--- Library/Install Rosetta 2.sh
+++ Library/Install Rosetta 2.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-if [ "$(/usr/bin/arch)" = "arm64" ]; then
-  /usr/sbin/softwareupdate --install-rosetta --agree-to-license
+arch=$(/usr/bin/arch)
+if [ "$arch" == "arm64" ]; then
+    echo "Apple Silicon - Installing Rosetta"
+    /usr/sbin/softwareupdate --install-rosetta --agree-to-license
+elif [ "$arch" == "i386" ]; then
+    echo "Intel - Skipping Rosetta"
+else
+    echo "Unknown Architecture"
 fi
