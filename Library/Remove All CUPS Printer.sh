51dbd801d248c4c22e1041dc766dd7af4dabdced
Anver Housseini <anver@anykey.ch>
2020-12-11 14:43:56 +0100

    Update Remove All CUPS Printer.sh

--- Library/Remove All CUPS Printer.sh
+++ Library/Remove All CUPS Printer.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-for file in /etc/cups/ppd/*; do
-  path="${file%.ppd}"
-  name="${path##*/}"
-  echo "=> Remove printer '${name}'"
-  lpadmin -x "${name}"
-done
+echo "=> Reset the printing system..."
+
+/System/Library/Frameworks/ApplicationServices.framework/Frameworks/PrintCore.framework/Versions/A/printtool --reset -f
