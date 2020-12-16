d56d38b0e1b0de27da6fdd370acb1553c3cc78bc
Anver Housseini <anver@anykey.ch>
2020-12-16 23:13:58 +0100

    Update Install macOS Mojave.sh

--- Library/Install macOS Mojave.sh
+++ Library/Install macOS Mojave.sh
@@ -1,6 +1,40 @@
 #!/bin/bash
 # shellcheck disable=SC2071
 
+recon_after_upgrade() {
+
+  jamf scheduledTask \
+    -command "/Library/Scripts/run_recon.sh" \
+    -name "reconreboot" \
+    -minute "*/5/"
+
+  cat >"/Library/Scripts/run_recon.sh" <<EOF
+#!/bin/bash
+
+if /usr/local/bin/jamf checkJSSConnection -retry 2; then
+
+  /usr/local/bin/jamf policy -event updateinventory
+
+  launchctl unload "/Library/LaunchDaemons/com.jamfsoftware.task.reconreboot.plist"
+
+  sleep 5
+
+  rm -f "/Library/LaunchDaemons/com.jamfsoftware.task.reconreboot.plist"
+  rm -f "/Library/Scripts/run_recon.sh"
+
+  exit 0
+
+fi
+
+else
+  exit 1
+fi
+EOF
+
+  chmod +x "/Library/Scripts/run_recon.sh"
+
+}
+
 installer_version="10.14"
 
 if [[ "$(sw_vers -buildVersion)" < "15" ]]; then
@@ -21,6 +55,8 @@ if [ -s "/usr/local/bin/erase-install" ]; then
       --os=${installer_version} \
       --catalogurl=https://swscan.apple.com/content/catalogs/others/index-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog
 
+    recon_after_upgrade
+
   else
 
     softwareupdate --reset-ignored
@@ -28,6 +64,8 @@ if [ -s "/usr/local/bin/erase-install" ]; then
     /usr/local/bin/erase-install \
       --reinstall \
       --os=${installer_version}
+
+    recon_after_upgrade
 
   fi
 
