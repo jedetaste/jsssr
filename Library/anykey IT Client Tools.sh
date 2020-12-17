df3f289317ec3beff22108f01403c003415945e1
Anver Housseini <anver@anykey.ch>
2020-12-17 15:01:46 +0100

    Update anykey IT Client Tools.sh

--- Library/anykey IT Client Tools.sh
+++ Library/anykey IT Client Tools.sh
@@ -547,7 +547,7 @@ elif [ -L "/usr/local/bin/erase-install" ]; then
   unlink "/usr/local/bin/erase-install"
 fi
 
-version="0.16.1"
+version="0.17.0"
 
 curl -s -L -o "/tmp/erase-install-${version}.zip" "https://github.com/grahampugh/erase-install/archive/v${version}.zip"
 
