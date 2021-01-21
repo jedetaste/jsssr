12c1c41a61fb8ea745cc3acf95b671d971634464
Anver Housseini <anver@anykey.ch>
2021-01-21 09:08:19 +0100

    Update Install Rosetta 2.sh

--- Library/Install Rosetta 2.sh
+++ Library/Install Rosetta 2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [ "$(/usr/bin/arch)" = "arm64" ]; then
-    if [[ ! -f "/Library/Apple/System/Library/LaunchDaemons/com.apple.oahd.plist" ]]; then
-      echo "=> This Mac runs on Apple Silicon, installing Rosetta..."
-      /usr/sbin/softwareupdate --install-rosetta --agree-to-license
-    else 
-        echo "Rosetta is already installed. Nothing to do."
-    fi
+  if [[ ! -f "/Library/Apple/System/Library/LaunchDaemons/com.apple.oahd.plist" ]]; then
+    echo "=> This Mac runs on Apple Silicon, installing Rosetta..."
+    /usr/sbin/softwareupdate --install-rosetta --agree-to-license
+  else
+    echo "Rosetta is already installed. Nothing to do."
+  fi
 
 elif [ "$(/usr/bin/arch)" = "i386" ]; then
   echo "=> This Mac runs on Intel"
