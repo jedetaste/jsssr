727b9296403bbe7981bf86b51b2ea5c2131e8df6
Anver Housseini <anver@anykey.ch>
2021-04-07 08:11:43 +0200

    Update Install GarageBand Loops.sh

--- Library/Install GarageBand Loops.sh
+++ Library/Install GarageBand Loops.sh
@@ -2,27 +2,27 @@
 
 if [ -d "/Applications/GarageBand.app" ]; then
 
-  echo "==> GarageBand.app is installed"
+  echo "=> GarageBand.app is installed"
 
   if [ -n "$(AssetCacheLocatorUtil 2>&1 | awk '/guid / { gsub(",", "", $4); print $4}' | uniq)" ]; then
     caching_server=$(AssetCacheLocatorUtil 2>&1 | awk '/guid / { gsub(",", "", $4); print $4}' | uniq)
-    echo "==> Found local caching server at '${caching_server}'"
+    echo "=> Found local caching server at '${caching_server}'"
   fi
 
   if [ -n "${caching_server}" ]; then
     if [ "${1}" = "optional" ]; then
-      "/usr/local/bin/appleLoops" --deployment -m -o --cache-server "http://${caching_server}"
+      "/usr/local/bin/appleloops" --deployment -m -o --cache-server "http://${caching_server}"
     else
-      "/usr/local/bin/appleLoops" --deployment -m --cache-server "http://${caching_server}"
+      "/usr/local/bin/appleloops" --deployment -m --cache-server "http://${caching_server}"
     fi
   else
     if [ "${1}" = "optional" ]; then
-      "/usr/local/bin/appleLoops" --deployment -m -o -q
+      "/usr/local/bin/appleloops" --deployment -m -o
     else
-      "/usr/local/bin/appleLoops" --deployment -m
+      "/usr/local/bin/appleloops" --deployment -m
     fi
   fi
 
 else
-  echo "==> GarageBand.app is not installed"
+  echo "=> GarageBand.app is not installed"
 fi
