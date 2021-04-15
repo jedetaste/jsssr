3a61f4be30b13c2e39fb08e4360fd27ce9794f59
Anver Housseini <anver@anykey.ch>
2021-04-15 09:13:57 +0200

    Update Remove All CUPS Printer.sh

--- Library/Remove All CUPS Printer.sh
+++ Library/Remove All CUPS Printer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # shellcheck disable=SC2071
 
-if [[ "$(sw_vers -buildVersion)" > "18" ]]; then
+if [[ "$(sw_vers -buildVersion)" < "18" ]]; then
 
   echo "=> Reset the printing system..."
   /System/Library/Frameworks/ApplicationServices.framework/Frameworks/PrintCore.framework/Versions/A/printtool --reset -f
