d8919d22b7bea9145a8aeca58cdf44ee553cb32e
Anver Housseini <anver@anykey.ch>
2021-04-15 09:14:54 +0200

    Update Adobe AIR EULA Acception.sh

--- Library/Adobe AIR EULA Acception.sh
+++ Library/Adobe AIR EULA Acception.sh
@@ -2,21 +2,42 @@
 
 # AdobeAIR EULA Acception for existing users
 
-for user in $(ls /Users | grep -v Shared | grep -v Guest | grep -v '.localized'); do
-  if [ ! -s "/Users/${user}/Library/Application Support/Adobe/AIR/eulaAccepted" ]; then
-    mkdir -p "/Users/${user}/Library/Application Support/Adobe/AIR/"
-    touch "/Users/${user}/Library/Application Support/Adobe/AIR/eulaAccepted"
-    echo "3" >>"/Users/${user}/Library/Application Support/Adobe/AIR/eulaAccepted"
-    chown -R "${user}" "/Users/${user}/Library/Application Support/Adobe/"
-    chmod -R 700 "/Users/${user}/Library/Application Support/Adobe/"
+cat <<EOF >"/tmp/stylesheet.xslt"
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+  <xsl:output method="text"/>
+  <xsl:template match="/">
+    <xsl:for-each select="users/user">
+      <xsl:value-of select="name"/>
+      <xsl:text>&#xa;</xsl:text>
+    </xsl:for-each>
+  </xsl:template>
+</xsl:stylesheet>
+EOF
+
+/usr/local/bin/jamf listUsers | xsltproc "/tmp/stylesheet.xslt" - | while read -r username; do
+
+  if [ ! -s "/Users/${username}/Library/Application Support/Adobe/AIR/eulaAccepted" ]; then
+
+    mkdir -p "/Users/${username}/Library/Application Support/Adobe/AIR/"
+    touch "/Users/${username}/Library/Application Support/Adobe/AIR/eulaAccepted"
+    echo "3" >>"/Users/${username}/Library/Application Support/Adobe/AIR/eulaAccepted"
+    chown -R "${username}" "/Users/${username}/Library/Application Support/Adobe/"
+    chmod -R 700 "/Users/$username/Library/Application Support/Adobe/"
+
   else
-    rm -f "/Users/${user}/Library/Application Support/Adobe/AIR/eulaAccepted"
-    touch "/Users/${user}/Library/Application Support/Adobe/AIR/eulaAccepted"
-    echo "3" >>"/Users/${user}/Library/Application Support/Adobe/AIR/eulaAccepted"
-    chown -R "${user}" "/Users/${user}/Library/Application Support/Adobe/"
-    chmod -R 700 "/Users/${user}/Library/Application Support/Adobe/"
+
+    rm -f "/Users/${username}/Library/Application Support/Adobe/AIR/eulaAccepted"
+    touch "/Users/${username}/Library/Application Support/Adobe/AIR/eulaAccepted"
+    echo "3" >>"/Users/${username}/Library/Application Support/Adobe/AIR/eulaAccepted"
+    chown -R "${username}" "/Users/${username}/Library/Application Support/Adobe/"
+    chmod -R 700 "/Users/${username}/Library/Application Support/Adobe/"
+
   fi
+
 done
+
+rm -f "/tmp/stylesheet.xslt"
 
 # AdobeAIR EULA Acception for user template
 
