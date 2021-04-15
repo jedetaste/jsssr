1072df78d5d6ae446554c87a672a2751799f3c81
Anver Housseini <anver@anykey.ch>
2021-04-15 09:13:28 +0200

    Update Install AdoptOpenJDK 8.sh

--- Library/Install AdoptOpenJDK 8.sh
+++ Library/Install AdoptOpenJDK 8.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 major_version="1.8.0"
-version="275"
-build="01"
+version="282"
+build="08"
 
-sha256="af005a2419b3dbc664fa3f73f2f43e9258da482110e73ff187566484102815f9"
+sha256="f12d380ceae806d02c4cae23bdc601402c543692c763122286b99d8ef6059794"
 download="https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u${version}-b${build}/OpenJDK8U-jdk_x64_mac_hotspot_8u${version}b${build}.pkg"
 
 tmp_dir=$(/usr/local/bin/tmpDir)
@@ -19,7 +19,7 @@ install_jdk() {
   curl -s -o "${tmp_dir}/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg" "${redirect_url}"
 
   if [ -s "${tmp_dir}/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg" ]; then
-    if [ "$(shasum -a 256 -p "${tmp_dir}/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg" | cut -d' ' -f1)" = "${sha256}" ]; then
+    if [ "$(shasum -a 256 "${tmp_dir}/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg" | cut -d' ' -f1)" = "${sha256}" ]; then
       echo "=> Expected SHA-256 checksum is correct"
       echo "=> Install '${tmp_dir}/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg'"
       installer -pkg "${tmp_dir}/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg" -target /
