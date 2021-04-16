29faad1852560d161cc463fdd37019c7bc307b69
Anver Housseini <anver@anykey.ch>
2021-04-16 10:11:14 +0200

    Update Install AdoptOpenJDK 8.sh

--- Library/Install AdoptOpenJDK 8.sh
+++ Library/Install AdoptOpenJDK 8.sh
@@ -7,7 +7,6 @@ build="08"
 sha256="f12d380ceae806d02c4cae23bdc601402c543692c763122286b99d8ef6059794"
 download="https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u${version}-b${build}/OpenJDK8U-jdk_x64_mac_hotspot_8u${version}b${build}.pkg"
 
-tmp_dir=$(/usr/local/bin/tmpDir)
 redirect_url=$(curl -w "%{redirect_url}" -o /dev/null -s "${download}")
 current_version=$(defaults read /Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Info.plist CFBundleGetInfoString 2>/dev/null)
 available_version="AdoptOpenJDK ${major_version}_${version}-b${build}"
@@ -16,13 +15,13 @@ install_jdk() {
 
   echo "=> Downloading '${download}'"
   echo "=> Downloading from '${redirect_url}'"
-  curl -s -o "${tmp_dir}/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg" "${redirect_url}"
+  curl -L -s -o "/tmp/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg" "${redirect_url}"
 
-  if [ -s "${tmp_dir}/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg" ]; then
-    if [ "$(shasum -a 256 "${tmp_dir}/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg" | cut -d' ' -f1)" = "${sha256}" ]; then
+  if [ -s "/tmp/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg" ]; then
+    if [ "$(shasum -a 256 "/tmp/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg" | cut -d' ' -f1)" = "${sha256}" ]; then
       echo "=> Expected SHA-256 checksum is correct"
-      echo "=> Install '${tmp_dir}/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg'"
-      installer -pkg "${tmp_dir}/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg" -target /
+      echo "=> Install '/tmp/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg'"
+      installer -pkg "/tmp/OpenJDK8U-jdk_x64_mac_hotspot_8u${212}b${build}.pkg" -target /
     else
       echo "SHA-256 checksum mismatch" && exit 1
     fi
@@ -45,4 +44,4 @@ else
   install_jdk "$@"
 fi
 
-rm -rf "${tmp_dir}"
+rm -rf "/tmp"
